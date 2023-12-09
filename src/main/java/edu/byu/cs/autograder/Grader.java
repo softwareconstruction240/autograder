9c483d1712e1717fb0ff7368589fd2e06ed22ad5
Paul Hathaway <paulchathaway@gmail.com>
2023-12-09 09:01:18 -0700

    improve variable naming and implement packageRepo and removeDirectory

--- src/main/java/edu/byu/cs/autograder/Grader.java
+++ src/main/java/edu/byu/cs/autograder/Grader.java
@@ -5,6 +5,11 @@ import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Comparator;
+import java.util.stream.Stream;
 
 /**
  * A template for fetching, compiling, and running student code
@@ -12,23 +17,55 @@ import java.io.File;
 public abstract class Grader {
 
     private final String repoUrl;
-    private final String localPath;
+    protected final String stagePath;
 
-    public Grader(String repoUrl, String localPath) {
+    protected final String studentRepoPath;
+
+    /**
+     * Creates a new grader
+     * @param repoUrl the url of the student repo
+     * @param stagePath the path for the student repo to be put in and tested
+     */
+    public Grader(String repoUrl, String stagePath) throws IOException {
         this.repoUrl = repoUrl;
-        this.localPath = localPath;
+        this.stagePath = new File(stagePath).getCanonicalPath();
+        this.studentRepoPath = new File(stagePath, "repo").getCanonicalPath();
     }
 
     public void run() {
-        fetchRepo(repoUrl, localPath);
-        compile();
+
+        removeDirectory();
+        fetchRepo(repoUrl);
+        packageRepo();
+        compileTests();
         runTests();
     }
 
-    private void fetchRepo(String repoUrl, String localPath) {
+    private void removeDirectory() {
+
+        File file = new File(stagePath);
+
+        if (!file.exists()) {
+            return;
+        }
+
+        try (Stream<Path> paths = Files.walk(file.toPath())) {
+            paths.sorted(Comparator.reverseOrder())
+                    .map(Path::toFile)
+                    .forEach(File::delete);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Fetches the student repo and puts it in the given local path
+     * @param repoUrl the url of the student repo
+     */
+    private void fetchRepo(String repoUrl) {
         CloneCommand cloneCommand = Git.cloneRepository()
                 .setURI(repoUrl)
-                .setDirectory(new File(localPath));
+                .setDirectory(new File(studentRepoPath));
 
         try (Git git = cloneCommand.call()) {
             System.out.println("Cloned repo to " + git.getRepository().getDirectory());
@@ -37,8 +74,35 @@ public abstract class Grader {
         }
     }
 
-    protected abstract void compile();
+    /**
+     * Packages the student repo into a jar
+     */
+    protected void packageRepo() {
+        String[] commands = new String[]{"compile", "package"};
 
+        for (String command : commands) {
+            ProcessBuilder processBuilder = new ProcessBuilder();
+            processBuilder.directory(new File(studentRepoPath));
+            processBuilder.command("mvn", command);
+            try {
+                processBuilder.inheritIO();
+                Process process = processBuilder.start();
+                int exitCode = process.waitFor();
+                assert exitCode == 0;
+            } catch (IOException | InterruptedException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Compiles the test files with the student code
+     */
+    protected abstract void compileTests();
+
+    /**
+     * Runs the tests on the student code
+     */
     protected abstract void runTests();
 
 }
