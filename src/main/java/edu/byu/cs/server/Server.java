0e9419da29a851f7a3c57e45f4b8b33b7b38158f
Paul Hathaway <paulchathaway@gmail.com>
2023-12-11 16:33:00 -0700

    add temp consts of repos for testing

--- src/main/java/edu/byu/cs/server/Server.java
+++ src/main/java/edu/byu/cs/server/Server.java
@@ -6,11 +6,14 @@ import edu.byu.cs.autograder.PhaseOneGrader;
 import java.io.IOException;
 
 public class Server implements Grader.Observer {
+
+    private static final String ALL_PASS_REPO = "https://github.com/leesjensen/chess.git";
+    private static final String ALL_FAIL_REPO = "https://github.com/softwareconstruction240/chess.git";
     public static void main(String[] args) {
 
         PhaseOneGrader grader;
         try {
-            grader = new PhaseOneGrader("https://github.com/softwareconstruction240/chess.git", "./stage", new Server());
+            grader = new PhaseOneGrader(ALL_FAIL_REPO, "./stage", new Server());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
