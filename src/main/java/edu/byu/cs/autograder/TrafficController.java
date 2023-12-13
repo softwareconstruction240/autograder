c0aa414f74c113cc1bf0552e285ff4324e3d3dd6
Paul Hathaway <paulchathaway@gmail.com>
2023-12-13 11:06:12 -0700

    use executor service

--- src/main/java/edu/byu/cs/autograder/TrafficController.java
+++ src/main/java/edu/byu/cs/autograder/TrafficController.java
@@ -1,6 +1,7 @@
 package edu.byu.cs.autograder;
 
-import java.util.concurrent.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class TrafficController {
     private final ExecutorService executorService = Executors.newFixedThreadPool(1);
