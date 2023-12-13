96c5ccac9baeb66679c224fe7ba94196ce786cbf
Paul Hathaway <paulchathaway@gmail.com>
2023-12-12 21:50:35 -0700

    remove observer implementation

--- src/main/java/edu/byu/cs/server/Server.java
+++ src/main/java/edu/byu/cs/server/Server.java
@@ -8,7 +8,7 @@ import edu.byu.cs.controller.WebSocketController;
 import static spark.Spark.init;
 import static spark.Spark.port;
 
-public class Server implements Grader.Observer {
+public class Server{
 
     private static final String ALL_PASS_REPO = "https://github.com/pawlh/chess-passing.git";
     private static final String ALL_FAIL_REPO = "https://github.com/softwareconstruction240/chess.git";
@@ -18,20 +18,5 @@ public class Server implements Grader.Observer {
         WebSocketController.registerRoute();
         RestController.registerRoutes();
         init();
-    }
-
-    @Override
-    public void update(String message) {
-        System.out.println(message);
-    }
-
-    @Override
-    public void notifyError(String message) {
-        System.err.println(message);
-    }
-
-    @Override
-    public void notifyDone(TestAnalyzer.TestNode results) {
-        System.out.println(results);
     }
 }
