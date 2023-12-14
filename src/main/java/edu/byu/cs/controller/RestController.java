b7eea2f5a979c6d0d7cb5f216f28172a16f6c858
Paul Hathaway <paulchathaway@gmail.com>
2023-12-14 12:07:17 -0700

    move initial auth to cas controller

--- src/main/java/edu/byu/cs/controller/RestController.java
+++ src/main/java/edu/byu/cs/controller/RestController.java
@@ -1,85 +1,38 @@
 package edu.byu.cs.controller;
 
-import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import edu.byu.cs.controller.security.CasController;
 
-import javax.net.ssl.HttpsURLConnection;
-import java.io.IOException;
-import java.net.URI;
-import java.util.Map;
-
+import static edu.byu.cs.controller.security.JwtUtils.validateToken;
 import static spark.Spark.*;
 
 public class RestController {
 
-    private static final String CAS_SERVER_URL = "https://cas.byu.edu/cas";
-    private static final String APP_URL = "http://localhost:8080";
-    private static final String SERVICE_VALIDATE_ENDPOINT = CAS_SERVER_URL + "/serviceValidate";
+
 
     public static void registerRoutes() {
 
         staticFiles.location("/public");
 
-        get("/callback", (req, res) -> {
-            String ticket = req.queryParams("ticket");
-
-            String netId = validateCasTicket(ticket);
-
-            if (netId != null)
-                return "You're authenticated!";
-
-            halt(400, "Ticket validation failed");
-            return null;
-        });
-
-        get("/login", (req, res) -> {
-            res.redirect(CAS_SERVER_URL + "/login?service=" + APP_URL + "/callback");
-            return null;
-        });
-
+        CasController.registerRoutes();
 
         // authenticated routes
         path("/api", () -> {
             before("/*", (req, res) -> {
-//              ... check if authenticated
-//              halt(401, "You are not welcome here");
-
+                String token = req.cookie("token");
+                if (token == null || !validateToken(token)) {
+                    res.redirect("/login", 302);
+                    halt(401);
+                }
             });
-            post("/logout", (req, res) -> "Bye World");
+
+            post("/logout", (req, res) -> {
+                res.removeCookie("token");
+                res.status(200);
+                return "You are logged out.";
+            });
 
 
         });
 
-    }
-
-    /**
-     * Validates a CAS ticket and returns the netId of the user if valid <br/>
-     * <a href="https://calnet.berkeley.edu/calnet-technologists/cas/how-cas-works">Berkeley CAS docs</a>
-     * @param ticket the ticket to validate
-     * @return the netId of the user if valid, null otherwise
-     * @throws IOException if there is an error with the CAS server response
-     */
-    private static String validateCasTicket(String ticket) throws IOException {
-        String validationUrl = SERVICE_VALIDATE_ENDPOINT +
-                "?ticket=" + ticket +
-                "&service=" + APP_URL + "/callback";
-
-        URI uri = URI.create(validationUrl);
-        HttpsURLConnection connection = (HttpsURLConnection) uri.toURL().openConnection();
-
-        try {
-            String body = new String(connection.getInputStream().readAllBytes());
-
-            Map casServiceResponse = XmlMapper
-                    .builder()
-                    .build().readValue(body, Map.class);
-            return (String) ((Map) casServiceResponse.get("authenticationSuccess")).get("user");
-
-        } catch (Exception e) {
-            System.err.println("Error with response from CAS server:");
-            e.printStackTrace();
-            throw e;
-        } finally {
-            connection.disconnect();
-        }
     }
 }
