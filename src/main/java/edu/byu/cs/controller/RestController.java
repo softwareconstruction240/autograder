bc3ee615dbf0eccfaae30d592decddc3cc30f329
Paul Hathaway <paulchathaway@gmail.com>
2023-12-14 00:24:34 -0700

    add basic cas authentication

--- src/main/java/edu/byu/cs/controller/RestController.java
+++ src/main/java/edu/byu/cs/controller/RestController.java
@@ -1,15 +1,39 @@
 package edu.byu.cs.controller;
 
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
+import javax.net.ssl.HttpsURLConnection;
+import java.io.IOException;
+import java.net.URI;
+import java.util.Map;
+
 import static spark.Spark.*;
 
 public class RestController {
+
+    private static final String CAS_SERVER_URL = "https://cas.byu.edu/cas";
+    private static final String APP_URL = "http://localhost:8080";
+    private static final String SERVICE_VALIDATE_ENDPOINT = CAS_SERVER_URL + "/serviceValidate";
+
     public static void registerRoutes() {
 
         //serve static files from resources /public. this is the official resources folder in java
         staticFiles.location("/public");
 
-        post("/api/login", (req, res) -> {
-            // ... validate credentials
+        get("/callback", (req, res) -> {
+            String ticket = req.queryParams("ticket");
+
+            String netId = validateCasTicket(ticket);
+
+            if (netId != null)
+                return "You're authenticated!";
+
+            halt(400, "Ticket validation failed");
+            return null;
+        });
+
+        get("/login", (req, res) -> {
+            res.redirect(CAS_SERVER_URL + "/login?service=" + APP_URL + "/callback");
             return null;
         });
 
@@ -26,5 +50,30 @@ public class RestController {
 
         });
 
+    }
+
+    private static String validateCasTicket(String ticket) throws IOException {
+        String validationUrl = SERVICE_VALIDATE_ENDPOINT +
+                "?ticket=" + ticket +
+                "&service=" + APP_URL + "/callback";
+
+        URI uri = URI.create(validationUrl);
+        HttpsURLConnection connection = (HttpsURLConnection) uri.toURL().openConnection();
+
+        try {
+            String body = new String(connection.getInputStream().readAllBytes());
+
+            Map casServiceResponse = XmlMapper
+                    .builder()
+                    .build().readValue(body, Map.class);
+            return (String) ((Map) casServiceResponse.get("authenticationSuccess")).get("user");
+
+        } catch (Exception e) {
+            System.err.println("Error with response from CAS server:");
+            e.printStackTrace();
+            throw e;
+        } finally {
+            connection.disconnect();
+        }
     }
 }
