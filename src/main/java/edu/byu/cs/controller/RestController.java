9ffac8b0f8483592495bf1847ffeda72eaf806b8
Paul Hathaway <paulchathaway@gmail.com>
2023-12-19 11:34:53 -0700

    pull in rest route registering to Server

--- src/main/java/edu/byu/cs/controller/RestController.java
+++ src/main/java/edu/byu/cs/controller/RestController.java
@@ -7,24 +7,4 @@ import static spark.Spark.*;
 
 public class RestController {
 
-
-
-    public static void registerRoutes() {
-
-        staticFiles.location("/public");
-
-        CasController.registerRoutes();
-
-        // authenticated routes
-        path("/api", () -> {
-            before("/*", (req, res) -> {
-                String token = req.cookie("token");
-                if (token == null || !validateToken(token)) {
-                    res.redirect("/login", 302);
-                    halt(401);
-                }
-            });
-        });
-
-    }
 }
--- src/main/java/edu/byu/cs/server/Server.java
+++ src/main/java/edu/byu/cs/server/Server.java
@@ -1,10 +1,9 @@
 package edu.byu.cs.server;
 
-import edu.byu.cs.controller.RestController;
 import edu.byu.cs.controller.WebSocketController;
 
-import static spark.Spark.init;
-import static spark.Spark.port;
+import static edu.byu.cs.controller.security.JwtUtils.validateToken;
+import static spark.Spark.*;
 
 public class Server{
 
@@ -14,7 +13,18 @@ public class Server{
 
         port(8080);
         WebSocketController.registerRoute();
-        RestController.registerRoutes();
+        webSocket("/ws", WebSocketController.class);
+        staticFiles.location("/public");
+
+        path("/api", () -> {
+            before("/*", (req, res) -> {
+                String token = req.cookie("token");
+                if (token == null || !validateToken(token)) {
+                    res.redirect("/login", 302);
+                    halt(401);
+                }
+            });
+        });
         init();
     }
 }
