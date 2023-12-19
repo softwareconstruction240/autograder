4cfdd6be482aab068cc53942068403c2105b1f40
Paul Hathaway <paulchathaway@gmail.com>
2023-12-19 11:53:53 -0700

    document auth middleware

--- src/main/java/edu/byu/cs/controller/AuthController.java
+++ src/main/java/edu/byu/cs/controller/AuthController.java
@@ -6,6 +6,10 @@ import static edu.byu.cs.controller.security.JwtUtils.validateToken;
 import static spark.Spark.halt;
 
 public class AuthController {
+    /**
+     * A Spark filter that verifies that the request has a valid JWT in the Authorization header.
+     * If the request is valid, the netId is added to the session for later use.
+     */
     public static Filter verifyAuthenticatedMiddleware = (req, res) -> {
         String token = req.headers("Authorization");
         if (token == null) {
