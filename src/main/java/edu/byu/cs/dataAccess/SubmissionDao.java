9dac7134113afa417c75cd3745018cd1634fdd7d
Paul Hathaway <paulchathaway@gmail.com>
2023-12-19 19:52:25 -0700

    add interface methods

--- src/main/java/edu/byu/cs/dataAccess/SubmissionDao.java
+++ src/main/java/edu/byu/cs/dataAccess/SubmissionDao.java
@@ -1,4 +1,30 @@
 package edu.byu.cs.dataAccess;
 
+import edu.byu.cs.model.Phase;
+import edu.byu.cs.model.Submission;
+
+import java.util.Collection;
+
 public interface SubmissionDao {
+
+    /**
+     * Inserts a new submission into the database
+     * @param submission the submission to insert
+     */
+    void insertSubmission(Submission submission);
+
+    /**
+     * Gets all submissions for the given netId and phase
+     * @param netId the netId to get submissions for
+     * @param phase the phase to get submissions for
+     * @return all submissions for the given netId and phase
+     */
+    Collection<Submission> getSubmissionsForPhase(String netId, Phase phase);
+
+    /**
+     * Gets all submissions for the given netId
+     * @param netId the netId to get submissions for
+     * @return all submissions for the given netId
+     */
+    Collection<Submission> getSubmissionsForUser(String netId);
 }
