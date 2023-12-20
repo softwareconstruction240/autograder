880c803bb7f8ab36eefad57b0e01622d99b6b950
Paul Hathaway <paulchathaway@gmail.com>
2023-12-19 23:46:31 -0700

    centralize fetch methods

--- src/main/resources/public/fetcher.js
+++ src/main/resources/public/fetcher.js
@@ -9,9 +9,22 @@ const fetcher = {
         return await res.json();
 
     },
+    register: async(firstName, lastName, repoUrl) => {
+        const urlParams = new URLSearchParams();
+        urlParams.append('firstName', firstName);
+        urlParams.append('lastName', lastName);
+        urlParams.append('repoUrl', repoUrl);
+        const res = await fetch('/auth/register', {
+            method: 'POST',
+            body: urlParams
+        });
+
+        if (res.status !== 200)
+            throw Error('Registration failed: ' + res.body);
+    },
     logout: async () => {
         // post to /logout
-        const res = await fetch('/logout', {method: 'POST'});
+        const res = await fetch('/auth/logout', {method: 'POST'});
         if (res.status !== 200)
             throw Error('Logout failed: ' + res.body);
     }
