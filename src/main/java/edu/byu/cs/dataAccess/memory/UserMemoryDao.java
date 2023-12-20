6044b7ac844b74d8ddd880172ab1ccdc8f898cc6
Paul Hathaway <paulchathaway@gmail.com>
2023-12-19 19:12:18 -0700

    implement UserDao in memory

---
+++ src/main/java/edu/byu/cs/dataAccess/memory/UserMemoryDao.java
@@ -0,0 +1,45 @@
+package edu.byu.cs.dataAccess.memory;
+
+import edu.byu.cs.dataAccess.UserDao;
+import edu.byu.cs.model.User;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class UserMemoryDao implements UserDao {
+
+    private static final ConcurrentHashMap<String, User> users = new ConcurrentHashMap<>();
+    @Override
+    public void insertUser(User user) {
+        if (users.containsKey(user.netId()))
+            throw new IllegalArgumentException("User already exists");
+
+        users.put(user.netId(), user);
+    }
+
+    @Override
+    public User getUser(String netId) {
+        return users.get(netId);
+    }
+
+    @Override
+    public void setRepoUrl(String netId, String repoUrl) {
+        if (!users.containsKey(netId))
+            throw new IllegalArgumentException("User does not exist");
+
+        User oldUser = users.get(netId);
+        User newUser = new User(oldUser.netId(), repoUrl, oldUser.role());
+
+        users.put(netId, newUser);
+    }
+
+    @Override
+    public void setRole(String netId, User.Role role) {
+        if (!users.containsKey(netId))
+            throw new IllegalArgumentException("User does not exist");
+
+        User oldUser = users.get(netId);
+        User newUser = new User(oldUser.netId(), oldUser.repoUrl(), role);
+
+        users.put(netId, newUser);
+    }
+}
