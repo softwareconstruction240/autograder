95624dffb43a8f26a90c7ef6ae1223a63aa88910
Paul Hathaway <paulchathaway@gmail.com>
2023-12-20 17:09:41 -0700

    add timestamp field and typeadapter

--- src/main/java/edu/byu/cs/model/Submission.java
+++ src/main/java/edu/byu/cs/model/Submission.java
@@ -1,13 +1,32 @@
 package edu.byu.cs.model;
 
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
 import edu.byu.cs.autograder.TestAnalyzer;
+
+import java.io.IOException;
+import java.time.Instant;
 
 public record Submission(
         String netId,
         String repoUrl,
         String headHash,
+        Instant timestamp,
         Phase phase,
         Float score,
         TestAnalyzer.TestNode testResults
 ) {
+    public static class InstantAdapter extends TypeAdapter<Instant> {
+
+        @Override
+        public void write(JsonWriter jsonWriter, Instant instant) throws IOException {
+            jsonWriter.value(instant.toString());
+        }
+
+        @Override
+        public Instant read(JsonReader jsonReader) throws IOException {
+            return Instant.parse(jsonReader.nextString());
+        }
+    }
 }
