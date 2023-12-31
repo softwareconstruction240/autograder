805235fd750a4deff20d9f680ce53d47f6d046b4
Paul Hathaway <paulchathaway@gmail.com>
2023-12-30 17:10:02 -0700

    create PhaseConfiguration model

---
+++ src/main/java/edu/byu/cs/model/PhaseConfiguration.java
@@ -0,0 +1,15 @@
+package edu.byu.cs.model;
+
+import java.time.ZonedDateTime;
+
+/**
+ * Represents a phase configuration. Contains information unique to a phase such as the due date.
+ * @param phase
+ * @param dueDate
+ */
+public record PhaseConfiguration(
+        Phase phase,
+        ZonedDateTime dueDate
+
+) {
+}
