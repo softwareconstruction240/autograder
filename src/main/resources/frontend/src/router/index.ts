a342c10ed31db879869808d09fc8036cf6a9fa2b
Paul Hathaway <paulchathaway@gmail.com>
2024-01-05 14:50:18 -0700

    add main routes

--- src/main/resources/frontend/src/router/index.ts
+++ src/main/resources/frontend/src/router/index.ts
@@ -1,4 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from "@/views/HomeView.vue";
+import LoginView from "@/views/LoginView.vue";
+import RegisterView from "@/views/RegisterView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -6,8 +9,18 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: () => null
+      component: HomeView
     },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: RegisterView
+    }
   ]
 })
 
