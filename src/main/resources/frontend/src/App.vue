ae3538e7c7fa91e0b461177c1b000eb3bf3c4992
Paul Hathaway <paulchathaway@gmail.com>
2024-01-05 14:50:43 -0700

    add app header

--- src/main/resources/frontend/src/App.vue
+++ src/main/resources/frontend/src/App.vue
@@ -2,7 +2,10 @@
 </script>
 
 <template>
-
+  <header>
+    <h1>CS 240 Autograder</h1>
+    <h3>This is where you can submit your assignments and view your scores.</h3>
+  </header>
   <router-view />
 </template>
 
