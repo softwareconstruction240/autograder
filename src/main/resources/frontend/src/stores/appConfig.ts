451f817a0e196094ec55c3788a8b6d8571e6249e
Paul Hathaway <paulchathaway@gmail.com>
2024-01-05 15:25:33 -0700

    rename backendHost to backendUrl

--- src/main/resources/frontend/src/stores/appConfig.ts
+++ src/main/resources/frontend/src/stores/appConfig.ts
@@ -2,13 +2,13 @@ import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
 type ImportMeta = {
-    VITE_APP_BACKEND_HOST: string
+    VITE_APP_BACKEND_URL: string
 }
 
 // @ts-ignore
 const env: ImportMeta = import.meta.env;
 export const useAppConfigStore = defineStore('appConfig', () => {
-  const backendHost = ref<string>(env.VITE_APP_BACKEND_HOST);
+  const backendUrl = ref<string>(env.VITE_APP_BACKEND_URL);
 
-  return { backendHost }
+  return { backendUrl }
 })
--- src/main/resources/frontend/src/views/LoginView.vue
+++ src/main/resources/frontend/src/views/LoginView.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
+import {useAppConfigStore} from "@/stores/appConfig";
 
+const login = () => {
+  window.location.href = useAppConfigStore().backendUrl + '/auth/login';
+}
 </script>
 
 <template>
-LoginView
+  <h2>You must log in before continuing</h2>
+  <p>After selecting Log In, you will be redirected to BYU's authentication page.</p>
+  <button @click="login">Log In</button>
 </template>
 
 <style scoped>
-
 </style>
