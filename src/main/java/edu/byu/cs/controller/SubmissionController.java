2766b99bd729cc8a83954678d28af831043ada5a
Paul Hathaway <paulchathaway@gmail.com>
2024-01-04 18:10:42 -0700

    move submit logic to SubmissionController

--- src/main/java/edu/byu/cs/controller/SubmissionController.java
+++ src/main/java/edu/byu/cs/controller/SubmissionController.java
@@ -1,16 +1,25 @@
 package edu.byu.cs.controller;
 
+import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonSyntaxException;
+import edu.byu.cs.autograder.*;
+import edu.byu.cs.controller.netmodel.GradeRequest;
 import edu.byu.cs.dataAccess.DaoService;
 import edu.byu.cs.model.Phase;
 import edu.byu.cs.model.Submission;
 import edu.byu.cs.model.User;
+import org.eclipse.jetty.websocket.api.Session;
 import spark.Route;
 
+import java.io.IOException;
 import java.time.Instant;
-import java.util.Collection;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
+
+import static edu.byu.cs.controller.WebSocketController.sendError;
+import static spark.Spark.halt;
 
 public class SubmissionController {
 
@@ -100,4 +109,97 @@ public class SubmissionController {
                 .registerTypeAdapter(Instant.class, new Submission.InstantAdapter())
                 .create().toJson(submissions);
     };
+
+    /**
+     * Creates a grader for the given request with an observer that sends messages to the subscribed sessions
+     *
+     * @param netId   the netId of the user
+     * @param request the request to create a grader for
+     * @return the grader
+     * @throws IOException if there is an error creating the grader
+     */
+    private static Grader getGrader(String netId, GradeRequest request) throws IOException {
+        Grader.Observer observer = new Grader.Observer() {
+            @Override
+            public void notifyStarted() {
+                queue.removeIf(queueNetId -> queueNetId.equals(netId));
+
+                sessions.get(netId).stream()
+                        .filter(Session::isOpen)
+                        .forEach(session -> WebSocketController.send(
+                                session,
+                                Map.of(
+                                        "type", "started"
+                                )));
+                broadcastQueueStatus();
+            }
+
+            @Override
+            public void update(String message) {
+                sessions.get(netId).stream()
+                        .filter(Session::isOpen)
+                        .forEach(session -> WebSocketController.send(
+                                session,
+                                Map.of(
+                                        "type", "update",
+                                        "message", message
+                                )));
+            }
+
+            @Override
+            public void notifyError(String message) {
+                sessions.get(netId).stream()
+                        .filter(Session::isOpen)
+                        .forEach(session -> sendError(
+                                session,
+                                message));
+
+                sessions.remove(netId);
+            }
+
+            @Override
+            public void notifyDone(TestAnalyzer.TestNode results) {
+                sessions.get(netId).stream()
+                        .filter(Session::isOpen)
+                        .forEach(session ->
+                                WebSocketController.send(
+                                        session,
+                                        Map.of(
+                                                "type", "results",
+                                                "results", new Gson().toJson(results)
+                                        )));
+
+                sessions.remove(netId);
+            }
+        };
+
+        return switch (request.phase()) {
+            case 0 -> new PhaseZeroGrader(request.repoUrl(), observer);
+            case 1 -> new PhaseOneGrader(request.repoUrl(), observer);
+            case 3 -> null;
+            case 4 -> null;
+            case 6 -> null;
+            default -> throw new IllegalStateException("Unexpected value: " + request.phase());
+        };
+    }
+
+    /**
+     * Broadcasts the current queue status to all connected clients.
+     * Each client will be notified of their specific position in the queue.
+     */
+    private static void broadcastQueueStatus() {
+        int i = 1;
+        for (String netId : queue) {
+            for (Session session : sessions.get(netId)) {
+                WebSocketController.send(
+                        session,
+                        Map.of(
+                                "type", "queueStatus",
+                                "position", i,
+                                "total", SubmissionController.queue.size()
+                        ));
+            }
+            i++;
+        }
+    }
 }
--- src/main/java/edu/byu/cs/controller/WebSocketController.java
+++ src/main/java/edu/byu/cs/controller/WebSocketController.java
@@ -1,23 +1,16 @@
 package edu.byu.cs.controller;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonSyntaxException;
-import edu.byu.cs.autograder.*;
-import edu.byu.cs.controller.netmodel.GradeRequest;
 import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.annotations.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.util.Arrays;
 import java.util.Map;
-import java.util.concurrent.ConcurrentLinkedQueue;
 
 @WebSocket
 public class WebSocketController {
     private static final Logger LOGGER = LoggerFactory.getLogger(WebSocketController.class);
-    private static final ConcurrentLinkedQueue<Session> queue = new ConcurrentLinkedQueue<>();
 
     @OnWebSocketConnect
     public void onConnect(Session session) {
@@ -26,122 +19,38 @@ public class WebSocketController {
 
     @OnWebSocketClose
     public void onClose(Session session, int statusCode, String reason) {
-        if (queue.remove(session))
-            broadcastQueueStatus();
-
-        LOGGER.info("Disconnected from " + session.getRemoteAddress() + ": " + reason);
     }
 
     @OnWebSocketError
     public void onError(Session session, Throwable t) {
-        if (session.isOpen() && queue.remove(session))
-            broadcastQueueStatus();
         LOGGER.warn("WebSocket error: ", t);
     }
 
     @OnWebSocketMessage
     public void onMessage(Session session, String message) {
         LOGGER.info("received: " + message);
-        if (queue.contains(session)) {
-            sendError(session, "You are already in the queue");
-            return;
-        }
 
-        GradeRequest request;
+        String netId = null;
         try {
-            request = new Gson().fromJson(message, GradeRequest.class);
-        } catch (JsonSyntaxException e) {
-            sendError(session, "Request must be valid json");
-            return;
-        }
-
-        // FIXME: improve git url validation
-        if (!request.repoUrl().matches("^https://[\\w.]+.\\w+/[\\w\\D]+/[\\w-/]+.git$")) {
-            sendError(session, "That doesn't look like a valid git url");
-            return;
-        }
-        if (!Arrays.asList(0, 1, 3, 4, 6).contains(request.phase())) {
-            sendError(session, "Valid phases are 0, 1, 3, 4, or 6");
-            return;
-        }
-
-        queue.add(session);
-        send(
-                session,
-                Map.of(
-                        "type", "queueStatus",
-                        "position", queue.size(),
-                        "total", queue.size()
-                )
-        );
-
-        try {
-            Grader grader = getGrader(session, request);
-
-            TrafficController.getInstance().addGrader(grader);
-
+            netId = JwtUtils.validateToken(message);
         } catch (Exception e) {
-            sendError(session, "Something went wrong");
+            LOGGER.warn("Exception thrown while validating token: ", e);
+
+            sendError(session, "Invalid token");
+            session.close();
+            return;
         }
 
+        if (!SubmissionController.sessions.containsKey(netId)) {
+            sendError(session, "You are not in the queue");
+            session.close();
+            return;
+        }
 
-    }
+        if (SubmissionController.sessions.get(netId).contains(session))
+            return;
 
-    /**
-     * Creates a grader for the given request with an observer that sends messages to the given session
-     *
-     * @param session the session to send messages to
-     * @param request the request to create a grader for
-     * @return the grader
-     * @throws IOException if there is an error creating the grader
-     */
-    private Grader getGrader(Session session, GradeRequest request) throws IOException {
-        Grader.Observer observer = new Grader.Observer() {
-            @Override
-            public void notifyStarted() {
-                queue.remove(session);
-                send(
-                        session,
-                        Map.of(
-                                "type", "started"
-                        ));
-                broadcastQueueStatus();
-            }
-
-            @Override
-            public void update(String message) {
-                send(
-                        session,
-                        Map.of(
-                                "type", "update",
-                                "message", message
-                        ));
-            }
-
-            @Override
-            public void notifyError(String message) {
-                sendError(session, message);
-            }
-
-            @Override
-            public void notifyDone(TestAnalyzer.TestNode results) {
-                send(
-                        session,
-                        Map.of(
-                                "type", "results",
-                                "results", new Gson().toJson(results)
-                        ));
-            }
-        };
-
-        return switch (request.phase()) {
-            case 0 -> new PhaseZeroGrader(request.repoUrl(), observer);
-            case 1 -> new PhaseOneGrader(request.repoUrl(), observer);
-            case 3 -> null;
-            case 4 -> null;
-            case 6 -> null;
-            default -> throw new IllegalStateException("Unexpected value: " + request.phase());
-        };
+        SubmissionController.sessions.get(netId).add(session);
     }
 
     /**
@@ -150,7 +59,7 @@ public class WebSocketController {
      * @param session the session to send the message to
      * @param message the message
      */
-    private void send(Session session, Map<String, Object> message) {
+    public static void send(Session session, Map<String, Object> message) {
         String jsonMessage = new Gson().toJson(message);
         LOGGER.info("Sending to " + session.getRemoteAddress() + ":\n" + jsonMessage);
         try {
@@ -166,7 +75,7 @@ public class WebSocketController {
      * @param session the session to send the message to
      * @param message the error message
      */
-    private void sendError(Session session, String message) {
+    public static void sendError(Session session, String message) {
         send(
                 session,
                 Map.of(
@@ -175,21 +84,5 @@ public class WebSocketController {
                 ));
     }
 
-    /**
-     * Broadcasts the current queue status to all connected clients.
-     * Each client will be notified of their specific position in the queue.
-     */
-    private void broadcastQueueStatus() {
-        int i = 1;
-        for (Session session : queue) {
-            send(
-                    session,
-                    Map.of(
-                            "type", "queueStatus",
-                            "position", i,
-                            "total", queue.size()
-                    ));
-            i++;
-        }
-    }
+
 }
