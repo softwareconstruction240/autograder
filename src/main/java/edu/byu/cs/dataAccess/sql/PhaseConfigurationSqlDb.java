2add9bee253f447302cecc7d64fe9d41a25c78eb
Paul Hathaway <paulchathaway@gmail.com>
2024-01-05 00:18:18 -0700

    implement dao methods

--- src/main/java/edu/byu/cs/dataAccess/sql/PhaseConfigurationSqlDb.java
+++ src/main/java/edu/byu/cs/dataAccess/sql/PhaseConfigurationSqlDb.java
@@ -1,20 +1,54 @@
 package edu.byu.cs.dataAccess.sql;
 
+import edu.byu.cs.dataAccess.DataAccessException;
 import edu.byu.cs.dataAccess.PhaseConfigurationDao;
 import edu.byu.cs.model.Phase;
 import edu.byu.cs.model.PhaseConfiguration;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.time.ZonedDateTime;
 
 public class PhaseConfigurationSqlDb implements PhaseConfigurationDao {
 
     @Override
     public void modifyDueDate(Phase phase, ZonedDateTime dueDate) {
-
+        try (var connection = SqlDb.getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(
+                    """
+                    UPDATE phase_configuration
+                    SET due_date_mountain_time = ?
+                    WHERE phase = ?
+                    """);
+            statement.setObject(1, dueDate);
+            statement.setString(2, phase.toString());
+            statement.executeUpdate();
+        } catch (Exception e) {
+            throw new DataAccessException("Error modifying due date", e);
+        }
     }
 
     @Override
     public PhaseConfiguration getPhaseConfiguration(Phase phase) {
-        return null;
+        try (var connection = SqlDb.getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(
+                    """
+                    SELECT phase, due_date_mountain_time
+                    FROM phase_configuration
+                    WHERE phase = ?
+                    """);
+            statement.setString(1, phase.toString());
+            ResultSet results = statement.executeQuery();
+            if (results.next()) {
+                return new PhaseConfiguration(
+                        Phase.valueOf(results.getString("phase")),
+                        results.getObject("due_date_mountain_time", ZonedDateTime.class)
+                );
+            } else {
+                return null;
+            }
+        } catch (Exception e) {
+            throw new DataAccessException("Error getting phase configuration", e);
+        }
     }
 }
