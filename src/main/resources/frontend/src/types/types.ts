40e158c38c7f6f7efb8e21f528b1801e015e978e
Paul Hathaway <paulchathaway@gmail.com>
2024-01-06 15:07:31 -0700

    add types file

---
+++ src/main/resources/frontend/src/types/types.ts
@@ -0,0 +1,21 @@
+export type Phase = '0' | '1' | '3' | '4' | '6';
+
+export type TestResult = {
+    testName: string,
+    passed: boolean,
+    errorMessage: string,
+    children: TestResult[],
+    numTestsPassed: number,
+    numTestsFailed: number,
+
+}
+
+export type Submission = {
+    netId: string,
+    repoUrl: string,
+    headHash: string,
+    timestamp: string,
+    phase: Phase,
+    score: number,
+    testResults: TestResult,
+}
