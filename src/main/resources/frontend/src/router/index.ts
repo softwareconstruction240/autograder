015d8227386154c4a89e5b09e237cf84a1b13e38
Paul Hathaway <paulchathaway@gmail.com>
2024-01-06 15:07:51 -0700

    remove unused import

--- src/main/resources/frontend/src/router/index.ts
+++ src/main/resources/frontend/src/router/index.ts
@@ -3,7 +3,6 @@ import HomeView from "@/views/HomeView.vue";
 import LoginView from "@/views/LoginView.vue";
 import RegisterView from "@/views/RegisterView.vue";
 import {useAuthStore} from "@/stores/auth";
-import PhaseView from "@/views/PhaseView.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
