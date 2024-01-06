c1ee4105158d269143c20f4cc9c750bee6195738
Paul Hathaway <paulchathaway@gmail.com>
2024-01-06 13:13:59 -0700

    run formatter

--- src/main/resources/frontend/src/router/index.ts
+++ src/main/resources/frontend/src/router/index.ts
@@ -1,32 +1,33 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 import HomeView from "@/views/HomeView.vue";
 import LoginView from "@/views/LoginView.vue";
 import RegisterView from "@/views/RegisterView.vue";
 import {useAuthStore} from "@/stores/auth";
+import PhaseView from "@/views/PhaseView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-      beforeEnter: (to, from) => {
-        if (!useAuthStore().isLoggedIn)
-            return '/login'
-      }
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: LoginView
-    },
-    {
-      path: '/register',
-      name: 'register',
-      component: RegisterView
-    }
-  ]
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: HomeView,
+            beforeEnter: (to, from) => {
+                if (!useAuthStore().isLoggedIn)
+                    return '/login'
+            }
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: LoginView
+        },
+        {
+            path: '/register',
+            name: 'register',
+            component: RegisterView
+        }
+    ]
 });
 
 export default router;
