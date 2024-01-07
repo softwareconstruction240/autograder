721f0805de6c840e52a0ee46b7d110943e51e804
Paul Hathaway <paulchathaway@gmail.com>
2024-01-07 13:49:27 -0700

    add component for displaying results

---
+++ src/main/resources/frontend/src/views/PhaseView/ResultsSection.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+import {readableTimestamp} from "@/utils/utils";
+import type {Submission, TestResult} from "@/types/types";
+
+const props = defineProps<{
+  submission: Submission;
+}>();
+
+const prettifyResults = (node: TestResult, indent: string) => {
+  if (!node) return "";
+
+  let result = indent + node.testName;
+
+  if (node.passed !== undefined) {
+    result += node.passed ? ` <span class="success">✓</span>` : ` <span class="failure">✗</span>`;
+    if (node.errorMessage !== null && node.errorMessage !== undefined && node.errorMessage !== "") {
+      result += "<br/>" + indent + "   Error: " + node.errorMessage;
+    }
+  } else {
+    result += ` (${node.numTestsPassed} passed, ${node.numTestsFailed} failed)`
+  }
+  result += "<br/>";
+
+  for (const key in node.children) {
+    if (node.children.hasOwnProperty(key)) {
+      result += prettifyResults(node.children[key], indent + "&nbsp;&nbsp;&nbsp;&nbsp;");
+    }
+  }
+
+  return result;
+}
+
+</script>
+
+<template>
+  <div class="container" v-if="submission !== undefined && submission !== null">
+    <div id="submission-title">
+      <h2>Results for {{ readableTimestamp(new Date(submission.timestamp)) }}</h2>
+    </div>
+    <div id="submission-notes">
+      some notes here
+    </div>
+    <div
+        id="submission-results-container"
+        v-html="prettifyResults(submission.testResults, '')"
+    ></div>
+  </div>
+</template>
+
+<style scoped>
+#submission-results-container {
+  /* sunken 3d */
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  padding: 10px;
+  margin: 10px;
+}
+</style>
