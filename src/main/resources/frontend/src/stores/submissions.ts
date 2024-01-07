a01eb63328e93abdb745f770dc684dbcff07c000
Paul Hathaway <paulchathaway@gmail.com>
2024-01-07 13:48:06 -0700

    add ws subscriber for grading updates

--- src/main/resources/frontend/src/stores/submissions.ts
+++ src/main/resources/frontend/src/stores/submissions.ts
@@ -2,6 +2,8 @@ import {defineStore} from "pinia";
 import {ref} from "vue";
 import type {Phase, Submission} from "@/types/types";
 import {submissionsGet} from "@/services/submissionService";
+import {useAppConfigStore} from "@/stores/appConfig";
+import {useAuthStore} from "@/stores/auth";
 
 type SubmissionsByPhase = {
     [phase: string]: Submission[];
@@ -19,3 +21,18 @@ export const useSubmissionStore = defineStore('submission', () => {
         getSubmissions
     };
 });
+
+export const subscribeToGradingUpdates = (eventHandler: (event: MessageEvent) => void) => {
+    const wsBackendUrl = useAppConfigStore().backendUrl.replace(/^http/, 'ws') + '/ws';
+    const ws = new WebSocket(wsBackendUrl);
+    ws.onopen = () => {
+        const token = useAuthStore().token;
+        ws.send(token);
+    };
+    ws.onmessage = (event) => {
+        eventHandler(event);
+    };
+    ws.onerror = (event) => {
+        console.error('ws error', event);
+    };
+}
