335b411fbb9e7561b127f4a7c313ca2d33ad731b
Paul Hathaway <paulchathaway@gmail.com>
2024-01-07 13:50:05 -0700

    add content for phases 0 and 1

--- src/main/resources/frontend/src/views/HomeView.vue
+++ src/main/resources/frontend/src/views/HomeView.vue
@@ -2,6 +2,7 @@
 
 import Tabs from "@/components/tabs/Tabs.vue";
 import Tab from "@/components/tabs/Tab.vue";
+import PhaseView from "@/views/PhaseView/PhaseView.vue";
 </script>
 
 <template>
@@ -9,8 +10,12 @@ import Tab from "@/components/tabs/Tab.vue";
     <h3 id="instruction-text">Select a phase from below to submit code, view test results, and review past attempts</h3>
   </div>
   <Tabs>
-    <Tab title="Phase 0">Phase 0 content</Tab>
-    <Tab title="Phase 1">Phase 1 content</Tab>
+    <Tab title="Phase 0">
+      <PhaseView phase-title="Phase 0: Chess piece move rules" phaseDescription="For this phase you must pass all of the provided automated tests." phase="0" />
+    </Tab>
+    <Tab title="Phase 1">
+      <PhaseView phase-title="Phase 1: Full chess game logic" phaseDescription="For this phase you must pass all of the provided automated tests." phase="1"/>
+    </Tab>
     <Tab title="Phase 3">Phase 3 content</Tab>
     <Tab title="Phase 4">Phase 4 content</Tab>
     <Tab title="Phase 6">Phase 6 content</Tab>
