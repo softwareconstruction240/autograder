6227ef24c8dda9ac8d97bae739166ff6e1745b87
Paul Hathaway <paulchathaway@gmail.com>
2024-01-11 16:39:57 -0700

    add store items for students and admins

--- src/main/resources/frontend/src/stores/admin.ts
+++ src/main/resources/frontend/src/stores/admin.ts
@@ -1,9 +1,13 @@
 import {defineStore} from "pinia";
 import type {User} from "@/types/types";
-import {ref} from "vue";
+import {computed, ref} from "vue";
 
 export const useAdminStore = defineStore('admin', () => {
     const users = ref<User[]>([]);
 
-    return {users}
+    const admins = computed(() => users.value.filter(user => user.role === 'ADMIN'));
+
+    const students = computed(() => users.value.filter(user => user.role === 'STUDENT'));
+
+    return {users, admins, students}
 });
