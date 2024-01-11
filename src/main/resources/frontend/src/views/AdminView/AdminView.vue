f0ae19d34d2a288e0ba0bdadbd9334e73b7381b8
Paul Hathaway <paulchathaway@gmail.com>
2024-01-11 11:18:16 -0700

    move AdminView to its own dir

---
+++ src/main/resources/frontend/src/views/AdminView/AdminView.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+
+import Tabs from "@/components/tabs/Tabs.vue";
+import Tab from "@/components/tabs/Tab.vue";
+</script>
+
+<template>
+  <Tabs>
+    <Tab title="Submissions">
+      //view and manage student submissions
+    </Tab>
+    <Tab title="Exceptions">
+      //view and manage student exceptions such as late submissions, insufficient commits, etc.
+    </Tab>
+    <Tab title="Assignment Options">
+      //view and manage assignment options such as due dates, late penalties, etc.
+    </Tab>
+    <Tab title="Admins">
+      //view and manage admins for the course
+    </Tab>
+  </Tabs>
+</template>
+
+<style scoped>
+</style>
