93ba4870a5a8d6599902f3f7123a0a5067e1f08a
Paul Hathaway <paulchathaway@gmail.com>
2024-01-11 20:24:47 -0700

    add service for updating users

--- src/main/resources/frontend/src/services/adminService.ts
+++ src/main/resources/frontend/src/services/adminService.ts
@@ -13,3 +13,41 @@ export const usersGet = async (): Promise<User[]> => {
         return [];
     }
 }
+
+interface UserPatch {
+    netId: string,
+    firstName?: string,
+    lastName?: string,
+    repoUrl?: string,
+    role?: string
+
+}
+
+/**
+ * this sends to /api/admin/user/{netId}
+ *
+ * query params can be zero or more of the following: firstName, lastName, repoUrl, role. these are added only if needed to the url
+ * @param user
+ */
+export const userPatch = async (user: UserPatch)=> {
+    const paramsString = new URLSearchParams();
+
+    if (user.firstName)
+        paramsString.append('firstName', user.firstName);
+    if (user.lastName)
+        paramsString.append('lastName', user.lastName);
+    if (user.repoUrl)
+        paramsString.append('repoUrl', user.repoUrl);
+    if (user.role)
+        paramsString.append('role', user.role);
+
+    try {
+        const response = await fetch(useAppConfigStore().backendUrl + '/api/admin/user/' + user.netId + '?' + paramsString.toString(), {
+            method: 'PATCH',
+            credentials: 'include'
+        });
+
+    } catch (e) {
+        console.error('Failed to update user: ', e);
+    }
+}
