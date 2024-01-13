90c3ddcc9e80db0f5313aac03be9fad2e042bfca
Paul Hathaway <paulchathaway@gmail.com>
2024-01-13 00:48:44 -0700

    add disabled prop

--- src/main/resources/frontend/src/components/tabs/TabInjectionKeys.ts
+++ src/main/resources/frontend/src/components/tabs/TabInjectionKeys.ts
@@ -3,6 +3,7 @@ import type {InjectionKey, Ref} from "vue";
 export interface Tab {
     title: string;
     hash: string;
+    disabled: boolean;
 }
 
 export const addTabKey = Symbol() as InjectionKey<(tab: Tab) => void>;
