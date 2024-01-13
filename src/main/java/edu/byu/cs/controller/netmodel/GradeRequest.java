ca9a79dabf565ae7d8001539009309d47845f0d9
Paul Hathaway <paulchathaway@gmail.com>
2024-01-13 15:57:22 -0700

    add getter for getting phase as enum type

--- src/main/java/edu/byu/cs/controller/netmodel/GradeRequest.java
+++ src/main/java/edu/byu/cs/controller/netmodel/GradeRequest.java
@@ -1,4 +1,9 @@
 package edu.byu.cs.controller.netmodel;
 
+import edu.byu.cs.model.Phase;
+
 public record GradeRequest(int phase, String repoUrl) {
+    public Phase getPhase() {
+        return Phase.valueOf("Phase" + phase);
+    }
 }
