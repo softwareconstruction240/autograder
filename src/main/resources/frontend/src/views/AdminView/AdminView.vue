0edf788706e5301db78b73403de0e0040b9609e4
Paul Hathaway <paulchathaway@gmail.com>
2024-01-17 13:58:04 -0700

    add option to activate test mode

--- src/main/resources/frontend/src/views/AdminView/AdminView.vue
+++ src/main/resources/frontend/src/views/AdminView/AdminView.vue
@@ -6,10 +6,21 @@ import SubmissionsView from "@/views/AdminView/Submissions.vue";
 import Exceptions from "@/views/AdminView/Exceptions.vue";
 import AssignmentOptions from "@/views/AdminView/AssignmentOptions.vue";
 import Admins from "@/views/AdminView/Admins/Admins.vue";
+import {testStudentModeGet} from "@/services/adminService";
+
+const activateTestStudentMode = async () => {
+  await testStudentModeGet()
+  window.location.href = '/';
+}
 </script>
 
 <template>
   <div class="container">
+    <div class="test-student-mode-container">
+      <p>Click here to become the test student. Note, you will need to log out and back in again to return to admin
+        mode.</p>
+      <button @click="activateTestStudentMode">Test Student Mode</button>
+    </div>
     <Tabs>
       <Tab title="Submissions">
         <SubmissionsView/>
@@ -35,5 +46,14 @@ import Admins from "@/views/AdminView/Admins/Admins.vue";
 <style scoped>
 .container {
   min-height: 50vh;
+}
+
+.test-student-mode-container {
+  margin-bottom: 20px;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  background-color: #f2f2f2;
+  cursor: pointer;
 }
 </style>
