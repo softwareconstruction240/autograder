af86f3d5e459ac874dd6fe70e3a455164628cc3f
Paul Hathaway <paulchathaway@gmail.com>
2024-01-23 13:11:52 -0700

    create table for queue

--- src/main/java/edu/byu/cs/dataAccess/sql/SqlDb.java
+++ src/main/java/edu/byu/cs/dataAccess/sql/SqlDb.java
@@ -65,6 +65,21 @@ public class SqlDb {
 
             connection.createStatement().executeUpdate(
                     """
+                            CREATE TABLE IF NOT EXISTS `queue` (
+                                `net_id` VARCHAR(20) NOT NULL,
+                                `phase` VARCHAR(9) NOT NULL,
+                                `time_added` DATETIME NOT NULL,
+                                PRIMARY KEY (`net_id`),
+                                CONSTRAINT `net_id`
+                                    FOREIGN KEY (`net_id`)
+                                    REFERENCES `user` (`net_id`)
+                                    ON DELETE CASCADE
+                                    ON UPDATE CASCADE
+                            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+                            """);
+
+            connection.createStatement().executeUpdate(
+                    """
                             CREATE TABLE IF NOT EXISTS `phase_configuration` (
                                 `phase` VARCHAR(9) NOT NULL,
                                 `due_date_mountain_time` DATETIME NOT NULL,
