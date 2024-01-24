1c23bc53e93342c9aa0ebaff6c72895239de9a86
Paul Hathaway <paulchathaway@gmail.com>
2024-01-23 17:27:08 -0700

    use bool for started field

--- src/main/java/edu/byu/cs/dataAccess/sql/SqlDb.java
+++ src/main/java/edu/byu/cs/dataAccess/sql/SqlDb.java
@@ -69,7 +69,7 @@ public class SqlDb {
                                 `net_id` VARCHAR(20) NOT NULL,
                                 `phase` VARCHAR(9) NOT NULL,
                                 `time_added` DATETIME NOT NULL,
-                                `time_started` DATETIME,
+                                `started` BOOL,
                                 PRIMARY KEY (`net_id`)
                             ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
                             """);
