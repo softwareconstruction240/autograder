11c290d6747fc669fa46b4e8fc99a9d15cffb21b
James Finlinson <the30clues@gmail.com>
2024-02-01 17:03:45 +0000

    Replace custom date formatting with builtin Date.toLocaleString()

--- src/main/resources/frontend/src/utils/utils.ts
+++ src/main/resources/frontend/src/utils/utils.ts
@@ -1,10 +1,4 @@
 export const readableTimestamp = (timestampOrString: Date | string) => {
   const timestamp = typeof timestampOrString === "string" ? new Date(timestampOrString) : timestampOrString;
-  // am/pm format
-  const hours = timestamp.getHours() > 12 ? timestamp.getHours() - 12 : timestamp.getHours();
-  const amPm = timestamp.getHours() > 12 ? 'PM' : 'AM';
-  const minutes = timestamp.getMinutes() < 10 ? `0${timestamp.getMinutes()}` : timestamp.getMinutes();
-    return `
-  ${timestamp.getMonth() + 1}/${timestamp.getDate()}/${timestamp.getFullYear()} ${hours}:${minutes} ${amPm}
-  `;
+  return timestamp.toLocaleString();
 }
