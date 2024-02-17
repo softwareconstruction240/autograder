bc200ac02c61d38714323cb3065fd2faa380f484
Paul Hathaway <paulchathaway@gmail.com>
2024-02-17 13:02:33 -0700

    add sub results item

--- src/main/java/edu/byu/cs/canvas/Rubric.java
+++ src/main/java/edu/byu/cs/canvas/Rubric.java
@@ -1,5 +1,7 @@
 package edu.byu.cs.canvas;
 
+import edu.byu.cs.autograder.TestAnalyzer;
+
 /**
  * Represents the rubric for a Canvas assignment. Some rubrics may have null values for some fields.
  *
@@ -13,11 +15,32 @@ public record Rubric(
         RubricItem quality
 ) {
 
+    /**
+     * Represents a single rubric item
+     *
+     * @param description The description of the rubric item
+     * @param results     The results of the rubric item
+     */
     record RubricItem(
             String description,
-            String notes,
-            Float score
+            Results results
 
+    ) {
+    }
+
+    /**
+     * Represents the results of a rubric item. textResults or testResults may be null, but not both
+     *
+     * @param notes
+     * @param score
+     * @param testResults
+     * @param textResults
+     */
+    record Results(
+            String notes,
+            Float score,
+            TestAnalyzer.TestNode testResults,
+            String textResults
     ) {
     }
 }
