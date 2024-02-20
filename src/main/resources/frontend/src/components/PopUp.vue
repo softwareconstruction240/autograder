2de8fdd93a9135cd3f60b5eb553e611fb1d77dcc
Paul Hathaway <paulchathaway@gmail.com>
2024-02-20 12:17:11 -0700

    create PopUp component

---
+++ src/main/resources/frontend/src/components/PopUp.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+import { defineEmits } from 'vue';
+
+defineEmits({
+  closePopUp: null,
+});
+</script>
+
+<template>
+  <div class="popup-overlay" @click="$emit('closePopUp')">
+    <div class="popup" @click.stop>
+      <button @click="$emit('closePopUp')" class="close-button">X</button>
+      <div class="popup-content">
+        <slot />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.popup-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.popup {
+  background: #fff;
+  padding: 20px;
+  border-radius: 8px;
+  position: relative;
+  max-height: 80vh;
+  overflow-y: auto;
+}
+
+.popup-content {
+  margin-bottom: 20px;
+  padding: 20px;
+}
+
+.close-button {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  cursor: pointer;
+}
+</style>
