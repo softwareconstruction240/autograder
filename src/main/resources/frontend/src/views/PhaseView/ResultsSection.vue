7ed8a18b40a59b79e1a37a6c9b9e75280d8ed031
Paul Hathaway <paulchathaway@gmail.com>
2024-02-19 21:04:15 -0700

    use RubricTable.vue

--- src/main/resources/frontend/src/views/PhaseView/ResultsSection.vue
+++ src/main/resources/frontend/src/views/PhaseView/ResultsSection.vue
@@ -1,57 +1,32 @@
 <script setup lang="ts">
-import {readableTimestamp} from "@/utils/utils";
-import type {Submission, TestResult} from "@/types/types";
+import Rubric from "@/views/PhaseView/RubricTable.vue";
+import type {Submission} from "@/types/types";
 
 defineProps<{
   submission: Submission;
 }>();
 
-const prettifyResults = (node: TestResult, indent: string) => {
-  if (!node) return "";
-
-  let result = indent + node.testName;
-
-  if (node.passed !== undefined) {
-    result += node.passed ? ` <span class="success">✓</span>` : ` <span class="failure">✗</span>`;
-    if (node.errorMessage !== null && node.errorMessage !== undefined && node.errorMessage !== "") {
-      result += `<br/>${indent}   ↳<span class="failure">${node.errorMessage}</span>`;
-    }
-  } else {
-    if (node.ecCategory !== undefined) {
-      result += ` (${node.numExtraCreditPassed} passed, ${node.numExtraCreditFailed} failed)`
-    } else {
-      result += ` (${node.numTestsPassed} passed, ${node.numTestsFailed} failed)`
-    }
-  }
-  result += "<br/>";
-
-  for (const key in node.children) {
-    if (node.children.hasOwnProperty(key)) {
-      result += prettifyResults(node.children[key], indent + "&nbsp;&nbsp;&nbsp;&nbsp;");
-    }
-  }
-
-  return result;
-}
-
 </script>
 
+<!--<template>-->
+<!--  <div class="container" v-if="submission !== undefined && submission !== null">-->
+<!--    <div id="submission-title">-->
+<!--      <h2>Results from {{ readableTimestamp(submission.timestamp) }}</h2>-->
+<!--    </div>-->
+<!--    <div id="submission-score">-->
+<!--      <h3>Score: {{ submission.score * 100 }}%</h3>-->
+<!--    </div>-->
+<!--    <div id="submission-notes">-->
+<!--      {{ submission.notes }}-->
+<!--    </div>-->
+<!--    <div-->
+<!--        id="submission-results-container"-->
+<!--        v-html="prettifyResults(submission.testResults, '')"-->
+<!--    ></div>-->
+<!--  </div>-->
+<!--</template>-->
 <template>
-  <div class="container" v-if="submission !== undefined && submission !== null">
-    <div id="submission-title">
-      <h2>Results from {{ readableTimestamp(submission.timestamp) }}</h2>
-    </div>
-    <div id="submission-score">
-      <h3>Score: {{ submission.score * 100 }}%</h3>
-    </div>
-    <div id="submission-notes">
-      {{ submission.notes }}
-    </div>
-    <div
-        id="submission-results-container"
-        v-html="prettifyResults(submission.testResults, '')"
-    ></div>
-  </div>
+  <Rubric :rubric="submission.rubric" />
 </template>
 
 <style scoped>
