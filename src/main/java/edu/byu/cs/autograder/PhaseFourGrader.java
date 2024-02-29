14f90637f6dddb98ecd5a030570e64037d408250
Paul Hathaway <paulchathaway@gmail.com>
2024-02-29 14:16:20 -0700

    init phase 4 grader

---
+++ src/main/java/edu/byu/cs/autograder/PhaseFourGrader.java
@@ -0,0 +1,36 @@
+package edu.byu.cs.autograder;
+
+import edu.byu.cs.model.Phase;
+import edu.byu.cs.model.Rubric;
+
+import java.io.IOException;
+
+import static edu.byu.cs.autograder.TestHelper.checkIfPassedPassoffTests;
+
+public class PhaseFourGrader extends PassoffTestGrader {
+    /**
+     * Creates a new grader for phase X
+     *
+     * @param netId          the netId of the student
+     * @param repoUrl        the url of the student repo
+     * @param observer       the observer to notify of updates
+     * @throws IOException if an IO error occurs
+     */
+    public PhaseFourGrader(String netId, String repoUrl, Observer observer) throws IOException {
+        super("./phases/phase4", netId, repoUrl, observer, Phase.Phase4);
+    }
+
+    @Override
+    protected boolean passed(Rubric rubric) {
+        return checkIfPassedPassoffTests(rubric);
+    }
+
+    @Override
+    protected String getCanvasRubricId(Rubric.RubricType type) {
+        return switch (type) {
+            case PASSOFF_TESTS -> "FIXME";
+            case UNIT_TESTS -> "FIXME";
+            case QUALITY -> "FIXME";
+        };
+    }
+}
