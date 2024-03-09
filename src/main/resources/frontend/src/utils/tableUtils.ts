9e55d6fd718738c789925bbd41895737dcf2f948
DallinFromEarth <112426674+DallinFromEarth@users.noreply.github.com>
2024-03-09 10:00:12 -0700

    add docs saying what fields are needed in ValueGetterParams

--- src/main/resources/frontend/src/utils/tableUtils.ts
+++ src/main/resources/frontend/src/utils/tableUtils.ts
@@ -2,25 +2,40 @@ import type { ValueGetterParams } from 'ag-grid-community'
 import {useAdminStore} from "@/stores/admin";
 import type {Submission} from "@/types/types";
 
+/**
+ * @param params is an ValueGetterParams Object from AG-Grid that contains a field called "phase"
+ */
 export const renderPhaseCell = (params: ValueGetterParams):string => {
     return params.data.phase.toLowerCase().replace("phase", "")
 }
 
+/**
+ * @param params is an ValueGetterParams Object from AG-Grid that contains a field called "netId"
+ */
 export const nameFromNetIdCellRender = (params: ValueGetterParams) => {
     const user = useAdminStore().usersByNetId[params.data.netId];
     return `${user.firstName} ${user.lastName}`
 }
 
+/**
+ * @param params is an ValueGetterParams Object from AG-Grid that contains a field called "timestamp"
+ */
 export const renderTimestampCell = (params: ValueGetterParams):string => {
     const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
     const time = new Date(params.data.timestamp)
     return months[time.getMonth()] + " " + time.getDate() + " " + time.getHours() + ":" + time.getMinutes()
 }
 
+/**
+ * @param params is an ValueGetterParams Object from AG-Grid that contains a field called "repoUrl"
+ */
 export const renderRepoLinkCell = (params: ValueGetterParams):string => {
     return '<a id="repo-link" href="' + params.data.repoUrl + '" target="_blank">' + params.data.repoUrl + '</a>'
 }
 
+/**
+ * @param params is an ValueGetterParams Object from AG-Grid that contains a field called "score"
+ */
 export const renderScoreCell = (params: ValueGetterParams):string => {
     return roundTwoDecimals(params.data.score * 100) + "%"
 }
