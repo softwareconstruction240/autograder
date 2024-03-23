deb386a41e88d1e146661d7d5543cfdd9595c7ad
Michael Davenport <mdaven19@byu.edu>
2024-03-23 15:19:46 -0600

    add rubric to assessment conversion

--- src/main/java/edu/byu/cs/canvas/CanvasUtils.java
+++ src/main/java/edu/byu/cs/canvas/CanvasUtils.java
@@ -1,14 +1,19 @@
 package edu.byu.cs.canvas;
 
+import edu.byu.cs.autograder.GradingException;
 import edu.byu.cs.dataAccess.DaoService;
 import edu.byu.cs.model.Phase;
 import edu.byu.cs.model.Rubric;
 import edu.byu.cs.model.RubricConfig;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CanvasUtils {
 
     /**
-     * Converts the score to points for each rubric item. E.g. if the rubric item is worth 10 points and the score is 0.5,
+     * Converts the score to points for each rubric item. E.g. if the rubric item is worth 10 points and the score is
+     * 0.5,
      * the points will be updated to 5
      *
      * @param phase  the phase of the rubric
@@ -68,5 +73,56 @@ public class CanvasUtils {
                 results.testResults(),
                 results.textResults()
         );
+    }
+
+    public static CanvasIntegration.RubricAssessment convertToAssessment(Rubric rubric, RubricConfig config,
+                                                                         float lateAdjustment, Phase phase)
+            throws GradingException {
+        Map<String, CanvasIntegration.RubricItem> items = new HashMap<>();
+        items.putAll(convertToCanvasFormat(rubric.passoffTests(), lateAdjustment, phase, config.passoffTests(),
+                Rubric.RubricType.PASSOFF_TESTS).items());
+        items.putAll(convertToCanvasFormat(rubric.unitTests(), lateAdjustment, phase, config.unitTests(),
+                Rubric.RubricType.UNIT_TESTS).items());
+        items.putAll(convertToCanvasFormat(rubric.quality(), lateAdjustment, phase, config.quality(),
+                Rubric.RubricType.QUALITY).items());
+        return new CanvasIntegration.RubricAssessment(items);
+    }
+
+    private static CanvasIntegration.RubricAssessment convertToCanvasFormat(Rubric.RubricItem rubricItem,
+                        float lateAdjustment, Phase phase, RubricConfig.RubricConfigItem rubricConfigItem,
+                        Rubric.RubricType rubricType) throws GradingException {
+        Map<String, CanvasIntegration.RubricItem> items = new HashMap<>();
+        if (rubricConfigItem != null && rubricConfigItem.points() > 0) {
+            Rubric.Results results = rubricItem.results();
+            items.put(getCanvasRubricId(rubricType, phase),
+                    new CanvasIntegration.RubricItem(results.notes(), results.score() * lateAdjustment));
+        }
+        return new CanvasIntegration.RubricAssessment(items);
+    }
+
+    private static String getCanvasRubricId(Rubric.RubricType type, Phase phase) throws GradingException {
+        return switch (phase) {
+            case Phase0, Phase1 -> switch (type) {
+                case PASSOFF_TESTS -> "_1958";
+                case UNIT_TESTS, QUALITY ->
+                        throw new GradingException(String.format("No %s item for this phase", type));
+            };
+            case Phase3 -> switch (type) {
+                case PASSOFF_TESTS -> "_5202";
+                case UNIT_TESTS -> "90344_776";
+                case QUALITY -> "_3003";
+            };
+            case Phase4 -> switch (type) {
+                case PASSOFF_TESTS -> "_2614";
+                case UNIT_TESTS -> "_930";
+                case QUALITY -> throw new GradingException(String.format("No %s item for this phase", type));
+            };
+            case Phase5 -> switch (type) {
+                case UNIT_TESTS -> "_8849";
+                case PASSOFF_TESTS, QUALITY ->
+                        throw new GradingException(String.format("No %s item for this phase", type));
+            };
+            case Phase6 -> throw new GradingException("Phase 6 not implemented yet");
+        };
     }
 }
