f55eff494fc04eec7fa22498f6771a2917264b6d
Paul Hathaway <paulchathaway@gmail.com>
2024-03-28 10:59:18 -0600

    init ScorerTest

---
+++ src/test/java/edu/byu/cs/autograder/score/ScorerTest.java
@@ -0,0 +1,21 @@
+package edu.byu.cs.autograder.score;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ScorerTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void score() {
+    }
+
+    @Test
+    void getScore() {
+
+    }
+}
