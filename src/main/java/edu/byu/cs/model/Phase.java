2e15e7178d523080972e458dbf194f169912cd52
James Finlinson <the30clues@gmail.com>
2024-04-02 09:47:54 -0600

    Move Phase configuration data into their own records...
    This shifts the emphasis on data from specific fields to the single object 'Phase'.
    This makes it easier to add new phases since all required fields are required to
    construct the appropriate 'Phase' object.
    
    This sets the stage for two other adjustments:
    1. Obviating and removing the PhaseUtils class
    2. Loading in the list of phases dynamically
       instead of changing code files to adjust
       the constants.
    
    This isn't the final step because we can't dynamically
    generate 'enum' types, but this is the first step
    in a series of transformations that will end up
    simplifying the responsibilites of course maintainers
    in the future.

--- src/main/java/edu/byu/cs/model/Phase.java
+++ src/main/java/edu/byu/cs/model/Phase.java
@@ -1,11 +1,143 @@
 package edu.byu.cs.model;
 
+import java.util.Map;
+import java.util.Set;
+
 public enum Phase {
-    Phase0,
-    Phase1,
-    Phase3,
-    Phase4,
-    Phase5,
-    Phase6,
-    Quality
+
+    // FIXME: Load this list of data dynamically from a configuration table #156
+
+    Phase0(
+            "0",
+            "0",
+            true,
+            null,
+            880445,
+            Map.of(Rubric.RubricType.PASSOFF_TESTS, "_1958"),
+            -1,
+            "shared",
+            null,
+            null,
+            Set.of("passoffTests.chessTests", "passoffTests.chessTests.chessPieceTests")
+    ),
+    Phase1(
+            "1",
+            "1",
+            true,
+            Phase0,
+            880446,
+            Map.of(Rubric.RubricType.PASSOFF_TESTS, "_1958"),
+            -1,
+            "shared",
+            null,
+            null,
+            Set.of("passoffTests.chessTests", "passoffTests.chessTests.chessExtraCredit")
+    ),
+    Phase3(
+            "3",
+            "3",
+            true,
+            Phase1,
+            880448,
+            Map.of(
+                Rubric.RubricType.PASSOFF_TESTS,  "_5202",
+                Rubric.RubricType.UNIT_TESTS,  "90344_776",
+                Rubric.RubricType.QUALITY,  "_3003"
+            ),
+            13,
+            "server",
+            "service",
+            Set.of("serviceTests"),
+            Set.of("passoffTests.serverTests")
+    ),
+    Phase4(
+            "4",
+            "4",
+            true,
+            Phase3,
+            880449,
+            Map.of(
+                Rubric.RubricType.PASSOFF_TESTS,  "_2614",
+                Rubric.RubricType.UNIT_TESTS,  "_930"
+            ),
+            18,
+            "server",
+            "dao",
+            Set.of("dataAccessTests"),
+            Set.of("passoffTests.serverTests")
+    ),
+    Phase5(
+            "5",
+            "5",
+            true,
+            Phase4,
+            880450,
+            Map.of(Rubric.RubricType.UNIT_TESTS,  "_8849"),
+            12,
+            "client",
+            "server facade",
+            Set.of("clientTests"),
+            null
+    ),
+    Phase6(
+            "6",
+            "6",
+            true,
+            Phase5,
+            880451,
+            null,
+            -1,
+            "server",
+            null,
+            null,
+            null
+    ),
+    Quality(
+            "42",
+            "Quality",
+            false,
+            null,
+            0,
+            null,
+            -1,
+            null,
+            null,
+            null,
+            null
+    );
+
+    // ## Value configuration
+
+    public final String stringValue;
+    public final String stringName;
+    public final boolean isGraded;
+    public final Phase previousPhase;
+
+    // Canvas config values
+    public final int assignmentNumber;
+    public final Map<Rubric.RubricType, String> rubricIds;
+
+    // Test configuration
+    public final int minUnitTests;
+    public final String moduleUnderTest;
+    public final String unitTestCodeUnderTest;
+    public final Set<String> unitTestPackagesToTest;
+    public final Set<String> passoffPackagesToTest;
+
+    Phase(String stringValue, String stringName, boolean isGraded, Phase previousPhase, int assignmentNumber,
+          Map<Rubric.RubricType, String> rubricIds, int minUnitTests, String moduleUnderTest,
+          String unitTestCodeUnderTest, Set<String> unitTestPackagesToTest, Set<String> passoffPackagesToTest
+    ) {
+        this.stringValue = stringValue;
+        this.stringName = stringName;
+        this.isGraded = isGraded;
+        this.previousPhase = previousPhase;
+        this.assignmentNumber = assignmentNumber;
+        this.rubricIds = rubricIds;
+        this.minUnitTests = minUnitTests;
+        this.moduleUnderTest = moduleUnderTest;
+        this.unitTestCodeUnderTest = unitTestCodeUnderTest;
+        this.unitTestPackagesToTest = unitTestPackagesToTest;
+        this.passoffPackagesToTest = passoffPackagesToTest;
+    }
 }
--- src/main/java/edu/byu/cs/util/PhaseUtils.java
+++ src/main/java/edu/byu/cs/util/PhaseUtils.java
@@ -3,19 +3,12 @@ package edu.byu.cs.util;
 import edu.byu.cs.autograder.GradingException;
 import edu.byu.cs.model.Phase;
 import edu.byu.cs.model.Rubric;
+import org.eclipse.jgit.annotations.NonNull;
 
+import java.util.Map;
 import java.util.Set;
 
 public class PhaseUtils {
-
-    // FIXME: dynamically get assignment numbers
-    private static final int PHASE0_ASSIGNMENT_NUMBER = 880445;
-    private static final int PHASE1_ASSIGNMENT_NUMBER = 880446;
-    private static final int PHASE3_ASSIGNMENT_NUMBER = 880448;
-    private static final int PHASE4_ASSIGNMENT_NUMBER = 880449;
-
-    private static final int PHASE5_ASSIGNMENT_NUMBER = 880450;
-    private static final int PHASE6_ASSIGNMENT_NUMBER = 880451;
 
     /**
      * Given a phase, returns the phase before it, or null.
@@ -24,14 +17,7 @@ public class PhaseUtils {
      * @return the previous phase chronologically
      */
     public static Phase getPreviousPhase(Phase phase) {
-        return switch (phase) {
-            case Phase0, Quality -> null;
-            case Phase1 -> Phase.Phase0;
-            case Phase3 -> Phase.Phase1;
-            case Phase4 -> Phase.Phase3;
-            case Phase5 -> Phase.Phase4;
-            case Phase6 -> Phase.Phase5;
-        };
+        return phase.previousPhase;
     }
 
     /**
@@ -41,15 +27,7 @@ public class PhaseUtils {
      * @return the string
      */
     public static String getPhaseAsString(Phase phase) {
-        return switch (phase) {
-            case Phase0 -> "0";
-            case Phase1 -> "1";
-            case Phase3 -> "3";
-            case Phase4 -> "4";
-            case Phase5 -> "5";
-            case Phase6 -> "6";
-            case Quality -> "Quality";
-        };
+        return phase.stringName;
     }
 
     /**
@@ -59,16 +37,8 @@ public class PhaseUtils {
      * @return the phase as an enum
      */
     public static Phase getPhaseByString(String str) {
-        return switch (str) {
-            case "0" -> Phase.Phase0;
-            case "1" -> Phase.Phase1;
-            case "3" -> Phase.Phase3;
-            case "4" -> Phase.Phase4;
-            case "5" -> Phase.Phase5;
-            case "6" -> Phase.Phase6;
-            case "42" -> Phase.Quality;
-            default -> null;
-        };
+        if (str.equals(Phase.Quality.stringName)) return Phase.Quality;
+        return Phase.valueOf("Phase" + str);
     }
 
     /**
@@ -78,93 +48,60 @@ public class PhaseUtils {
      * @return its assignment number in Canvas
      */
     public static int getPhaseAssignmentNumber(Phase phase) {
-        return switch (phase) {
-            case Phase0 -> PHASE0_ASSIGNMENT_NUMBER;
-            case Phase1 -> PHASE1_ASSIGNMENT_NUMBER;
-            case Phase3 -> PHASE3_ASSIGNMENT_NUMBER;
-            case Phase4 -> PHASE4_ASSIGNMENT_NUMBER;
-            case Phase5 -> PHASE5_ASSIGNMENT_NUMBER;
-            case Phase6 -> PHASE6_ASSIGNMENT_NUMBER;
-            case Quality -> 0;
-        };
+        return phase.assignmentNumber;
     }
 
     public static Set<String> passoffPackagesToTest(Phase phase) throws GradingException {
-        return switch (phase) {
-            case Phase0 -> Set.of("passoffTests.chessTests", "passoffTests.chessTests.chessPieceTests");
-            case Phase1 -> Set.of("passoffTests.chessTests", "passoffTests.chessTests.chessExtraCredit");
-            case Phase3, Phase4 -> Set.of("passoffTests.serverTests");
-            case Phase5, Quality -> throw new GradingException("No passoff tests for this phase");
-            case Phase6 -> throw new GradingException("Not implemented");
-        };
+        var val = phase.passoffPackagesToTest;
+        if (val == null) {
+            throw new GradingException("No passoff tests for this phase");
+        }
+        return val;
     }
 
     public static Set<String> unitTestPackagesToTest(Phase phase) throws GradingException {
-        return switch (phase) {
-            case Phase0, Phase1, Phase6, Quality -> throw new GradingException("No unit tests for this phase");
-            case Phase3 -> Set.of("serviceTests");
-            case Phase4 -> Set.of("dataAccessTests");
-            case Phase5 -> Set.of("clientTests");
-        };
+        var val = phase.unitTestPackagesToTest;
+        if (val == null) {
+            throw new GradingException("No unit tests for this phase");
+        }
+        return val;
     }
 
     public static String unitTestCodeUnderTest(Phase phase) throws GradingException {
-        return switch (phase) {
-            case Phase0, Phase1, Phase6, Quality -> throw new GradingException("No unit tests for this phase");
-            case Phase3 -> "service";
-            case Phase4 -> "dao";
-            case Phase5 -> "server facade";
-        };
+        var val = phase.unitTestCodeUnderTest;
+        if (val == null) {
+            throw new GradingException("No unit tests for this phase");
+        }
+        return val;
     }
 
     public static int minUnitTests(Phase phase) throws GradingException {
-        return switch (phase) {
-            case Phase0, Phase1, Phase6, Quality -> throw new GradingException("No unit tests for this phase");
-            case Phase3 -> 13;
-            case Phase4 -> 18;
-            case Phase5 -> 12;
-        };
+        var val = phase.minUnitTests;
+        if (val < 0) {
+            throw new GradingException("No unit tests for this phase");
+        }
+        return val;
     }
 
+    @NonNull
     public static String getCanvasRubricId(Rubric.RubricType type, Phase phase) throws GradingException {
-        return switch (phase) {
-            case Phase0, Phase1 -> switch (type) {
-                case PASSOFF_TESTS -> "_1958";
-                case UNIT_TESTS, QUALITY -> throw new GradingException(String.format("No %s item for this phase", type));
-            };
-            case Phase3 -> switch (type) {
-                case PASSOFF_TESTS -> "_5202";
-                case UNIT_TESTS -> "90344_776";
-                case QUALITY -> "_3003";
-            };
-            case Phase4 -> switch (type) {
-                case PASSOFF_TESTS -> "_2614";
-                case UNIT_TESTS -> "_930";
-                case QUALITY -> throw new GradingException(String.format("No %s item for this phase", type));
-            };
-            case Phase5 -> switch (type) {
-                case UNIT_TESTS -> "_8849";
-                case PASSOFF_TESTS, QUALITY -> throw new GradingException(String.format("No %s item for this phase", type));
-            };
-            case Phase6 -> throw new GradingException("Phase 6 not implemented yet");
-            case Quality -> throw new GradingException("Not graded");
-        };
+        var rubricIds = phase.rubricIds;
+        if (rubricIds == null) {
+            throw new GradingException("Phase is not graded in Canvas");
+        }
+
+        var val = rubricIds.get(type);
+        if (val == null) {
+            throw new GradingException(String.format("No %s item for this phase", type));
+        }
+        return val;
     }
 
     public static String getModuleUnderTest(Phase phase) {
-        //FIXME : Not sure what's wrong with this but there was a empty fixme comment when I refactored -Michael
-        return switch (phase) {
-            case Phase0, Phase1 -> "shared";
-            case Phase3, Phase4, Phase6 -> "server";
-            case Phase5 -> "client";
-            case Quality -> null;
-        };
+        return phase.moduleUnderTest;
     }
 
     public static boolean isPhaseGraded(Phase phase) {
-        return switch (phase) {
-            case Phase0, Phase1, Phase3, Phase4, Phase5, Phase6 -> true;
-            case Quality -> false;
-        };
+        return phase.isGraded;
     }
 }
