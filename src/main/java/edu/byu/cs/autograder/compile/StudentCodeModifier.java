646fe254c39361125c52c4ed8e1500c2e53278ba
Michael Davenport <mdaven19@byu.edu>
2024-04-03 23:00:50 +0000

    remove old import

--- src/main/java/edu/byu/cs/autograder/compile/StudentCodeModifier.java
+++ src/main/java/edu/byu/cs/autograder/compile/StudentCodeModifier.java
@@ -3,8 +3,6 @@ package edu.byu.cs.autograder.compile;
 import edu.byu.cs.autograder.GradingContext;
 import edu.byu.cs.autograder.GradingException;
 
-import java.io.File;
-
 public interface StudentCodeModifier {
     void modifyCode(GradingContext context) throws GradingException;
 }
