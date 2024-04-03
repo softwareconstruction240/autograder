76e8932498402507bf687ade351f664c2ef80247
Michael Davenport <52420978+19mdavenport@users.noreply.github.com>
2024-04-02 18:48:13 -0600

    Update ProcessUtils.java

--- src/main/java/edu/byu/cs/util/ProcessUtils.java
+++ src/main/java/edu/byu/cs/util/ProcessUtils.java
@@ -5,7 +5,7 @@ import java.util.concurrent.*;
 
 public class ProcessUtils {
 
-    private static final long DEFAULT_TIMEOUT = 30000;
+    private static final long DEFAULT_TIMEOUT = 120000;
 
     /**
      * Runs a process given by a process builder and returns process output
