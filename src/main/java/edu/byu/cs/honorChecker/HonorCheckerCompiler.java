89fcaa6fb80111d1808b98b243f3ebe265c3a241
Noah Pratt <prattnj@gmail.com>
2024-04-09 20:10:31 -0600

    Rename honor checker .zips

--- src/main/java/edu/byu/cs/honorChecker/HonorCheckerCompiler.java
+++ src/main/java/edu/byu/cs/honorChecker/HonorCheckerCompiler.java
@@ -37,7 +37,7 @@ public class HonorCheckerCompiler {
         try {
             for (User student : students) {
                 if (student.firstName().equals("Test") && student.lastName().equals("Student")) continue;
-                File repoPath = new File(tmpDir, student.netId());
+                File repoPath = new File(tmpDir, String.join("_", student.firstName(), student.lastName(), student.netId()));
 
                 CloneCommand cloneCommand = Git.cloneRepository()
                         .setURI(student.repoUrl())
