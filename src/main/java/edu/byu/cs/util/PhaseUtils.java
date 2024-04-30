3a5f56a6fe02419a1dbcb7d610d45613d93567da
19mdavenport <michaeldaven19@hotmail.com>
2024-04-30 23:04:24 +0000

    update assignment numbers

--- src/main/java/edu/byu/cs/util/PhaseUtils.java
+++ src/main/java/edu/byu/cs/util/PhaseUtils.java
@@ -9,13 +9,13 @@ import java.util.Set;
 public class PhaseUtils {
 
     // FIXME: dynamically get assignment numbers
-    private static final int PHASE0_ASSIGNMENT_NUMBER = 880445;
-    private static final int PHASE1_ASSIGNMENT_NUMBER = 880446;
-    private static final int PHASE3_ASSIGNMENT_NUMBER = 880448;
-    private static final int PHASE4_ASSIGNMENT_NUMBER = 880449;
+    private static final int PHASE0_ASSIGNMENT_NUMBER = 921303;
+    private static final int PHASE1_ASSIGNMENT_NUMBER = 921304;
+    private static final int PHASE3_ASSIGNMENT_NUMBER = 921306;
+    private static final int PHASE4_ASSIGNMENT_NUMBER = 921307;
 
-    private static final int PHASE5_ASSIGNMENT_NUMBER = 880450;
-    private static final int PHASE6_ASSIGNMENT_NUMBER = 880451;
+    private static final int PHASE5_ASSIGNMENT_NUMBER = 921308;
+    private static final int PHASE6_ASSIGNMENT_NUMBER = 921309;
 
     /**
      * Given a phase, returns the phase before it, or null.
