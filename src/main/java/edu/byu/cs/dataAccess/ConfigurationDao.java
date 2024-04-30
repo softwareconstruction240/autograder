470bacdddf17fe2311cc548e5b8619a50ca2ea87
19mdavenport <michaeldaven19@hotmail.com>
2024-04-30 21:35:06 +0000

    update config enum add course settings

--- src/main/java/edu/byu/cs/dataAccess/ConfigurationDao.java
+++ src/main/java/edu/byu/cs/dataAccess/ConfigurationDao.java
@@ -5,6 +5,15 @@ public interface ConfigurationDao {
     <T> T getConfiguration(Configuration key, Class<T> type) throws DataAccessException;
 
     enum Configuration {
-        STUDENT_SUBMISSION_ENABLED
+        COURSE_NUMBER,
+        STUDENT_SUBMISSION_START,
+        STUDENT_SUBMISSION_END,
+        GIT_REPO_ASSIGNMENT_NUMBER,
+        PHASE0_ASSIGNMENT_NUMBER,
+        PHASE1_ASSIGNMENT_NUMBER,
+        PHASE3_ASSIGNMENT_NUMBER,
+        PHASE4_ASSIGNMENT_NUMBER,
+        PHASE5_ASSIGNMENT_NUMBER,
+        PHASE6_ASSIGNMENT_NUMBER,
     }
 }
