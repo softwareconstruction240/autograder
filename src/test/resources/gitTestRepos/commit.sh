ec699723f3a1ec2beb1ca6b4caa5f32cf2d1617c
James Finlinson <the30clues@gmail.com>
2024-05-03 03:12:34 -0600

    Generalize commit.sh to accept dates and num line changes

--- src/test/resources/gitTestRepos/commit.sh
+++ src/test/resources/gitTestRepos/commit.sh
@@ -1,14 +1,21 @@
 #! /bin/bash
 
-# Usage: sh ../commit.sh "Commit message" [<DAYS AGO>]
+# Usage: sh ../commit.sh "Commit message" [[<DATE_VALUE> [NUM_LINES]]
 # Use within the sub directors to change a file with a given commit message
 # Change will be to the named file.
 # If provided, the days ago flag will set the commit to N days ago
 
 file="file.txt"
 CONTENT=$1
-DAYS_AGO=$2
-[ -z "$DAYS_AGO" ] && DAYS_AGO="0"
+DATE_VALUE=$2
+NUM_LINES=$3
+[ -z "$DATE_VALUE" ] && DATE_VALUE="0 days ago"
+[ -z "$NUM_LINES" ] && NUM_LINES="20"
 
 echo "$CONTENT" > $file
-git commit -m "$CONTENT" --date="$DAYS_AGO days ago" -- $file
+for ((i=2;i<=NUM_LINES;i++)); do
+    echo "$CONTENT" >> $file
+done
+
+
+git commit -m "$CONTENT" --date="$DATE_VALUE" -- $file
