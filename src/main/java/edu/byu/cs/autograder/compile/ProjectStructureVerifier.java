8d940db972a05c244212a18c746489d238a3b508
19mdavenport <michaeldaven19@hotmail.com>
2024-05-08 01:00:39 +0000

    add directory verification

--- src/main/java/edu/byu/cs/autograder/compile/ProjectStructureVerifier.java
+++ src/main/java/edu/byu/cs/autograder/compile/ProjectStructureVerifier.java
@@ -2,6 +2,7 @@ package edu.byu.cs.autograder.compile;
 
 import edu.byu.cs.autograder.GradingContext;
 import edu.byu.cs.autograder.GradingException;
+import edu.byu.cs.model.Phase;
 
 import java.io.File;
 
@@ -12,9 +13,35 @@ import java.io.File;
 public class ProjectStructureVerifier implements StudentCodeModifier {
     @Override
     public void modifyCode(GradingContext context) throws GradingException {
+        verifyRootPom(context);
+        verifyDirectoryStructure(context);
+    }
+
+    private void verifyRootPom(GradingContext context) throws GradingException {
         File pomFile = new File(context.stageRepo(), "pom.xml");
         if (!pomFile.exists()) {
             throw new GradingException("Project is not structured correctly. Your project should be at the top level of your git repository.");
+        }
+    }
+
+    private void verifyDirectoryStructure(GradingContext context) {
+        verifyDirectory(context, "shared/src/main/java");
+        verifyDirectory(context, "shared/src/test/java");
+        verifyDirectory(context, "server/src/main/java");
+        verifyDirectory(context, "client/src/main/java");
+        if(context.phase() == Phase.Phase0 || context.phase() == Phase.Phase1) return;
+
+        verifyDirectory(context, "server/src/test/java");
+        verifyDirectory(context,  "server/src/main/resources");
+        if(context.phase() == Phase.Phase3 || context.phase() == Phase.Phase4) return;
+
+        verifyDirectory(context,  "client/src/test/java");
+    }
+
+    private void verifyDirectory(GradingContext context, String filePath) {
+        File file = new File(context.stageRepo(), filePath);
+        if(!file.exists() || !file.isDirectory()) {
+            context.observer().notifyWarning("Directory %s could not be found".formatted(filePath));
         }
     }
 }
