d473fc6d6096a56f38a68090c52d2b23ef619096
19mdavenport <michaeldaven19@hotmail.com>
2024-05-09 20:40:07 +0000

    add static import check

--- src/main/java/edu/byu/cs/autograder/compile/ModuleIndependenceVerifier.java
+++ src/main/java/edu/byu/cs/autograder/compile/ModuleIndependenceVerifier.java
@@ -5,7 +5,6 @@ import edu.byu.cs.autograder.GradingException;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Path;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -14,44 +13,58 @@ import java.util.stream.Collectors;
 public class ModuleIndependenceVerifier implements StudentCodeVerifier {
     @Override
     public void verify(GradingContext context, StudentCodeReader reader) throws GradingException {
-        Set<File> serverFiles = reader.filesMatching(".*server/src/main/java/.*").collect(Collectors.toSet());
-        Path serverPath = context.stageRepo().toPath().resolve("server/src/main/java");
-        Set<String> serverClasses = qualifiedNames(serverFiles, serverPath);
-
-        Set<File> clientFiles = reader.filesMatching(".*client/src/main/java/.*").collect(Collectors.toSet());
-        Path clientPath = context.stageRepo().toPath().resolve("client/src/main/java");
-        Set<String> clientClasses = qualifiedNames(clientFiles, clientPath);
-
         try {
-            checkImports(context, reader, serverFiles, clientClasses);
-            checkImports(context, reader, clientFiles, serverClasses);
+            Set<File> serverFiles = reader.filesMatching(".*server/src/main/java/.*").collect(Collectors.toSet());
+            Set<String> serverPackages = packageNames(serverFiles, reader);
+
+            Set<File> clientFiles = reader.filesMatching(".*client/src/main/java/.*").collect(Collectors.toSet());
+            Set<String> clientPackages = packageNames(clientFiles, reader);
+
+            checkImports(context, reader, serverFiles, clientPackages);
+            checkImports(context, reader, clientFiles, serverPackages);
         } catch (IOException e) {
             throw new GradingException("Unable to verify module independence", e);
         }
     }
 
-    private Set<String> qualifiedNames(Set<File> files, Path relativeTo) {
-        Set<String> classes = new HashSet<>();
+    private Set<String> packageNames(Set<File> files, StudentCodeReader reader) throws IOException {
+        Set<String> packages = new HashSet<>();
         for(File file : files) {
-            String fileName = relativeTo.relativize(file.toPath()).toString();
-            if(!fileName.contains(".java")) continue;
-            classes.add(fileName.substring(0, fileName.indexOf('.')).replace('/', '.'));
+            if(!file.getName().contains(".java")) continue;
+            List<String> contents = reader.getFileContents(file);
+            if(contents.isEmpty()) continue;
+            String firstLine = contents.getFirst();
+            if(firstLine.startsWith("package")) {
+                packages.add(firstLine.substring(8, firstLine.indexOf(';')));
+            }
         }
-        return classes;
+        return packages;
     }
 
-    private void checkImports(GradingContext context, StudentCodeReader reader, Set<File> files, Set<String> classes)
+    private void checkImports(GradingContext context, StudentCodeReader reader, Set<File> files, Set<String> packages)
             throws IOException {
         for(File file : files) {
             List<String> contents = reader.getFileContents(file);
-            for (String line : contents) {
-                if (line.matches("(?:abstract )?class|interface|record")) break;
-                for (String classImport : classes) {
-                    if (line.matches("import %s;".formatted(classImport))) {
-                        String warning = "File %s imports %s. The client and server modules should be independent"
-                                .formatted(context.stageRepo().toPath().relativize(file.toPath()), classImport);
-                        context.observer().notifyWarning(warning);
-                    }
+            for (int i = 0; i < contents.size(); i++) {
+                String line = contents.get(i).trim();
+                if (line.isEmpty()) continue;
+                if (!(line.startsWith("package") || line.startsWith("import"))) {
+                    break;
+                }
+
+                String packageImport = line.substring(line.indexOf(' ')).trim();
+                if (packageImport.startsWith("static")) {
+                    packageImport = packageImport.substring(packageImport.indexOf(' '), packageImport.lastIndexOf('.')).trim();
+                }
+                if (packageImport.contains(".")) {
+                    packageImport = packageImport.substring(0, packageImport.lastIndexOf('.'));
+                }
+
+                if (packages.contains(packageImport)) {
+                    String warning = ("File %s imports from package %s (line %d), which exists in another module. " +
+                            "The client and server modules should be independent")
+                            .formatted(context.stageRepo().toPath().relativize(file.toPath()), packageImport, i + 1);
+                    context.observer().notifyWarning(warning);
                 }
             }
         }
