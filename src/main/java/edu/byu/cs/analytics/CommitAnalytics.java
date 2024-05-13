ed59c033c78870739dffa0bcbf22995fcffb1cc1
James Finlinson <the30clues@gmail.com>
2024-05-13 17:49:00 -0600

    Correct counting of changes in root commits.
    Also optimize the counting of changes in commits.
    While streams worked just fine, they created a lot
    of unnecessary overhead and duplicated containers.
    Manually iterating the patches ourselves results
    in significant improvements to run time with large
    numbers of patches in each commit.

--- src/main/java/edu/byu/cs/analytics/CommitAnalytics.java
+++ src/main/java/edu/byu/cs/analytics/CommitAnalytics.java
@@ -17,10 +17,7 @@ import org.eclipse.jgit.annotations.Nullable;
 import org.eclipse.jgit.api.CloneCommand;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
-import org.eclipse.jgit.diff.DiffEntry;
-import org.eclipse.jgit.diff.DiffFormatter;
-import org.eclipse.jgit.diff.Edit;
-import org.eclipse.jgit.diff.RawTextComparator;
+import org.eclipse.jgit.diff.*;
 import org.eclipse.jgit.errors.IncorrectObjectTypeException;
 import org.eclipse.jgit.errors.MissingObjectException;
 import org.eclipse.jgit.lib.ObjectId;
@@ -33,6 +30,7 @@ import org.eclipse.jgit.util.io.DisabledOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
+import java.util.function.ToIntFunction;
 
 /**
  * Analyzes the commit history of every student with a GitHub repo URL submission
@@ -287,20 +285,25 @@ public class CommitAnalytics {
      * @throws IOException When the system can't read the data properly.
      */
     private static int getNumChangesInCommit(DiffFormatter diffFormatter, RevCommit revCommit) throws IOException {
+        RevCommit comparisonCommit;
         int parentCount = revCommit.getParentCount();
-        if (parentCount == 0) {
-            return 0; // Root commit doesn't have any changes
-        } else if (parentCount > 1) {
+        if (parentCount == 1) {
+            comparisonCommit = revCommit.getParent(0);
+        } else if (parentCount == 0) {
+            comparisonCommit = null; // Root commits can still have changes
+        } else {
             throw new IllegalArgumentException("Cannot count changes in a merge commit.");
         }
 
-        List<DiffEntry> diffs = diffFormatter.scan(revCommit.getParent(0), revCommit);
+        List<DiffEntry> diffs = diffFormatter.scan(comparisonCommit, revCommit);
 
         int totalChanges = 0;
         for (var diff : diffs) {
             FileHeader fileHeader = diffFormatter.toFileHeader(diff);
-            // https://archive.eclipse.org/jgit/docs/jgit-2.0.0.201206130900-r/apidocs/org/eclipse/jgit/diff/Edit.html
-            totalChanges += fileHeader.toEditList().stream().mapToInt(e -> e.getLengthA() + e.getLengthB()).sum();
+            for (var edit : fileHeader.toEditList()) {
+                // https://archive.eclipse.org/jgit/docs/jgit-2.0.0.201206130900-r/apidocs/org/eclipse/jgit/diff/Edit.html
+                totalChanges += edit.getLengthA() + edit.getLengthB();
+            }
         }
 
         return totalChanges;
