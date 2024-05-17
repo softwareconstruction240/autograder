7804f8ed754bb9beaf7b0cfcbac35832ee6a616d
19mdavenport <michaeldaven19@hotmail.com>
2024-05-17 18:32:16 +0000

    fix regex

--- src/main/java/edu/byu/cs/autograder/compile/ModuleIndependenceVerifier.java
+++ src/main/java/edu/byu/cs/autograder/compile/ModuleIndependenceVerifier.java
@@ -53,7 +53,7 @@ public class ModuleIndependenceVerifier implements StudentCodeVerifier {
             for (int i = 0; i < contents.size(); i++) {
                 String line = contents.get(i).trim();
                 if (line.isEmpty()) continue;
-                if (line.matches("^(?!import|package|//).*\\b(class|record|enum|@?interface)\\b")) break;
+                if (line.matches("^(/\\*\\*|@|(?!import|package|//).*\\b(class|record|enum|@?interface)\\b).*$")) break;
 
                 String packageImport = line.substring(line.indexOf(' ')).trim();
                 if (packageImport.startsWith("static")) {
