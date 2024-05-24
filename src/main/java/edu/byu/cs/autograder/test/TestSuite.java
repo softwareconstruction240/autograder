326736ba030859a64b3be8b014b59bd1a81e3e54
19mdavenport <michaeldaven19@hotmail.com>
2024-05-24 20:57:09 +0000

    add baseline xml parse target class

---
+++ src/main/java/edu/byu/cs/autograder/test/TestSuite.java
@@ -0,0 +1,66 @@
+package edu.byu.cs.autograder.test;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlCData;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlText;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class TestSuite {
+    @JacksonXmlElementWrapper(useWrapping = false)
+    List<TestCase> testcase = new ArrayList<>();
+
+    public List<TestCase> getTestcase() {
+        return testcase;
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class TestCase {
+        @JacksonXmlProperty(isAttribute = true)
+        private String name;
+
+        @JacksonXmlProperty(isAttribute = true)
+        private String classname;
+
+        @JsonProperty("failure")
+        private CData failure;
+
+        @JsonProperty("system-out")
+        private CData systemOut;
+
+        public String getName() {
+            return name;
+        }
+
+        public String getClassname() {
+            return classname;
+        }
+
+        public CData getFailure() {
+            return failure;
+        }
+
+        public CData getSystemOut() {
+            return systemOut;
+        }
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class CData {
+        @JacksonXmlCData
+        @JacksonXmlText
+        private String data;
+
+        public String getData() {
+            return data;
+        }
+    }
+}
