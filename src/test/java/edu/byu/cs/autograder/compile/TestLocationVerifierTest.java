b6d3a3bb8b1c9f24f0dd2b4f24745fd965ca4aa8
Isaih Barron <isaihbarron@gmail.com>
2024-05-30 19:01:28 +0000

    Add placeholder test

---
+++ src/test/java/edu/byu/cs/autograder/compile/TestLocationVerifierTest.java
@@ -0,0 +1,22 @@
+package edu.byu.cs.autograder.compile;
+
+public class TestLocationVerifierTest {
+
+    /*
+     * Placeholder for this test in the future
+     * Example repos used in testing:
+     * https://github.com/Fiwafoofa/chessTA/tree/missing-test-directories
+     * Deletes: client/src/test/java/client, server/src/test/java/passoff
+     *
+     * https://github.com/Fiwafoofa/chessTA/tree/bad-test-locations
+     * Wrong package name: client/src/test/java/clientImABadStudent/ServerFacadeTests.java,
+     *    server/src/test/java/serviceImABadStudent/ClearServiceTests.java,
+     *    server/src/test/java/serviceImABadStudent/UserServiceTests.java
+     * Missing package name: server/src/test/java/serviceImABadStudent/GameServiceTests.java
+     *
+     * Random package: client/src/test/java/someRandomTestPackage/yoooooooooooo.java
+     *
+     * Wrongly named packages: server/src/test/java/serviceImABadStudent
+     */
+
+}
