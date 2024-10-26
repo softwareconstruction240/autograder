967c395e24f4d12fb68a5bb6ea4821831cfcf91d
ThanGerlek <nathaniel.gerlek@gmail.com>
2024-10-26 04:55:00 +0000

    refactor: split UserController into handler and service classes

---
+++ src/main/java/edu/byu/cs/controller/InternalServerException.java
@@ -0,0 +1,15 @@
+package edu.byu.cs.controller;
+
+public class InternalServerException extends Exception {
+    public InternalServerException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public InternalServerException(String message) {
+        super(message);
+    }
+
+    public InternalServerException(Throwable cause) {
+        super(cause);
+    }
+}
---
+++ src/main/java/edu/byu/cs/controller/PriorRepoClaimBlockageException.java
@@ -0,0 +1,14 @@
+package edu.byu.cs.controller;
+
+public class PriorRepoClaimBlockageException extends Exception {
+    private static final String secrets = "btw im a teapot";
+    public PriorRepoClaimBlockageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public PriorRepoClaimBlockageException(String message) {
+        super(message);
+    }
+    public PriorRepoClaimBlockageException(Throwable cause) {
+        super(cause);
+    }
+}
