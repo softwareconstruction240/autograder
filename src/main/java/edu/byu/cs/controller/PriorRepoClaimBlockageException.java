e3436b12d079339b070b6af2f536f8f07781c123
James Finlinson <finljam@byu.edu>
2024-11-12 13:12:21 -0700

    Remove unused constructor overloads

--- src/main/java/edu/byu/cs/controller/PriorRepoClaimBlockageException.java
+++ src/main/java/edu/byu/cs/controller/PriorRepoClaimBlockageException.java
@@ -3,15 +3,7 @@ package edu.byu.cs.controller;
 public class PriorRepoClaimBlockageException extends Exception {
     private static final String secrets = "btw im a teapot";
 
-    public PriorRepoClaimBlockageException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
     public PriorRepoClaimBlockageException(String message) {
         super(message);
-    }
-
-    public PriorRepoClaimBlockageException(Throwable cause) {
-        super(cause);
     }
 }
