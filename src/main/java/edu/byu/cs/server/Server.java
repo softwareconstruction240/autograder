476dd402872dc7c0136c779d13eb0e49c59264ef
Nathaniel Gerlek <nathaniel.gerlek@gmail.com>
2024-11-14 10:05:59 -0700

    refactor: rmv Server's dependency on Controllers

--- src/main/java/edu/byu/cs/server/Server.java
+++ src/main/java/edu/byu/cs/server/Server.java
@@ -5,6 +5,8 @@ import edu.byu.cs.controller.WebSocketController;
 import edu.byu.cs.dataAccess.DaoService;
 import edu.byu.cs.dataAccess.DataAccessException;
 import edu.byu.cs.properties.ApplicationProperties;
+import edu.byu.cs.server.endpointprovider.EndpointProvider;
+import edu.byu.cs.server.endpointprovider.EndpointProviderImpl;
 import edu.byu.cs.service.SubmissionService;
 import edu.byu.cs.util.ResourceUtils;
 import org.apache.commons.cli.*;
@@ -15,19 +17,18 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Properties;
 
-import static edu.byu.cs.controller.AdminController.*;
-import static edu.byu.cs.controller.AuthController.*;
-import static edu.byu.cs.controller.CasController.*;
-import static edu.byu.cs.controller.ConfigController.*;
-import static edu.byu.cs.controller.SubmissionController.*;
-import static edu.byu.cs.controller.UserController.*;
 import static spark.Spark.*;
 
 public class Server {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Server.class);
+    private final EndpointProvider provider;
 
-    public static int setupEndpoints(int port) {
+    public Server(EndpointProvider endpointProvider) {
+        this.provider = endpointProvider;
+    }
+
+    public int setupEndpoints(int port) {
         port(port);
 
         webSocket("/ws", WebSocketController.class);
@@ -43,79 +44,77 @@ public class Server {
         });
 
         path("/auth", () -> {
-            get("/callback", callbackGet);
-            get("/login", loginGet);
+            get("/callback", provider.callbackGet());
+            get("/login", provider.loginGet());
 
             // all routes after this point require authentication
-            post("/logout", logoutPost);
+            post("/logout", provider.logoutPost());
         });
 
         path("/api", () -> {
             before("/*", (req, res) -> {
-                if (!req.requestMethod().equals("OPTIONS"))
-                    verifyAuthenticatedMiddleware.handle(req, res);
+                if (!req.requestMethod().equals("OPTIONS")) provider.verifyAuthenticatedMiddleware().handle(req, res);
             });
 
-            patch("/repo", repoPatch);
+            patch("/repo", provider.repoPatch());
 
-            get("/submit", submitGet);
-            post("/submit", submitPost);
+            get("/submit", provider.submitGet());
+            post("/submit", provider.submitPost());
 
-            get("/latest", latestSubmissionForMeGet);
+            get("/latest", provider.latestSubmissionForMeGet());
 
-            get("/submission", submissionXGet);
-            get("/submission/:phase", submissionXGet);
+            get("/submission", provider.submissionXGet());
+            get("/submission/:phase", provider.submissionXGet());
 
-            get("/me", meGet);
+            get("/me", provider.meGet());
 
-            get("/config", getConfigStudent);
+            get("/config", provider.getConfigStudent());
 
             path("/admin", () -> {
                 before("/*", (req, res) -> {
-                    if (!req.requestMethod().equals("OPTIONS"))
-                        verifyAdminMiddleware.handle(req, res);
+                    if (!req.requestMethod().equals("OPTIONS")) provider.verifyAdminMiddleware().handle(req, res);
                 });
 
-                patch("/repo/:netId", repoPatchAdmin);
+                patch("/repo/:netId", provider.repoPatchAdmin());
 
-                get("/repo/history", repoHistoryAdminGet);
+                get("/repo/history", provider.repoHistoryAdminGet());
 
-                get("/users", usersGet);
+                get("/users", provider.usersGet());
 
-                post("/submit", adminRepoSubmitPost);
+                post("/submit", provider.adminRepoSubmitPost());
 
                 path("/submissions", () -> {
-                    post("/approve", approveSubmissionPost);
+                    post("/approve", provider.approveSubmissionPost());
 
-                    get("/latest", latestSubmissionsGet);
+                    get("/latest", provider.latestSubmissionsGet());
 
-                    get("/latest/:count", latestSubmissionsGet);
+                    get("/latest/:count", provider.latestSubmissionsGet());
 
-                    get("/active", submissionsActiveGet);
+                    get("/active", provider.submissionsActiveGet());
 
-                    get("/student/:netId", studentSubmissionsGet);
+                    get("/student/:netId", provider.studentSubmissionsGet());
 
-                    post("/rerun", submissionsReRunPost);
+                    post("/rerun", provider.submissionsReRunPost());
                 });
 
-                get("/test_mode", testModeGet);
+                get("/test_mode", provider.testModeGet());
 
-                get("/analytics/commit", commitAnalyticsGet);
+                get("/analytics/commit", provider.commitAnalyticsGet());
 
-                get("/analytics/commit/:option", commitAnalyticsGet);
+                get("/analytics/commit/:option", provider.commitAnalyticsGet());
 
-                get("/honorChecker/zip/:section", honorCheckerZipGet);
+                get("/honorChecker/zip/:section", provider.honorCheckerZipGet());
 
-                get("/sections", sectionsGet);
+                get("/sections", provider.sectionsGet());
 
                 path("/config", () -> {
-                    get("", getConfigAdmin);
+                    get("", provider.getConfigAdmin());
 
-                    post("/phases", updateLivePhases);
-                    post("/banner", updateBannerMessage);
+                    post("/phases", provider.updateLivePhases());
+                    post("/banner", provider.updateBannerMessage());
 
-                    post("/courseIds", updateCourseIdsPost);
-                    get("/courseIds", updateCourseIdsUsingCanvasGet);
+                    post("/courseIds", provider.updateCourseIdsPost());
+                    get("/courseIds", provider.updateCourseIdsUsingCanvasGet());
                 });
             });
         });
@@ -207,7 +206,8 @@ public class Server {
             throw new RuntimeException(e);
         }
 
-        int port = setupEndpoints(8080);
+        EndpointProvider endpointProvider = new EndpointProviderImpl();
+        int port = new Server(endpointProvider).setupEndpoints(8080);
 
         LOGGER.info("Server started on port {}", port);
 
---
+++ src/main/java/edu/byu/cs/server/endpointprovider/EndpointProvider.java
@@ -0,0 +1,55 @@
+package edu.byu.cs.server.endpointprovider;
+
+import spark.Filter;
+import spark.Route;
+
+public interface EndpointProvider {
+
+    // AdminController
+
+    Route usersGet();
+    Route testModeGet();
+    Route commitAnalyticsGet();
+    Route honorCheckerZipGet();
+    Route sectionsGet();
+
+    // AuthController
+
+    Filter verifyAuthenticatedMiddleware();
+    Filter verifyAdminMiddleware();
+    Route meGet();
+
+    // CasController
+
+    Route callbackGet();
+    Route loginGet();
+    Route logoutPost();
+
+    // ConfigController
+
+    Route getConfigAdmin();
+    Route getConfigStudent();
+    Route updateLivePhases();
+    Route updateBannerMessage();
+    Route updateCourseIdsPost();
+    Route updateCourseIdsUsingCanvasGet();
+
+    // SubmissionController
+
+    Route submitPost();
+    Route adminRepoSubmitPost();
+    Route submitGet();
+    Route latestSubmissionForMeGet();
+    Route submissionXGet();
+    Route latestSubmissionsGet();
+    Route submissionsActiveGet();
+    Route studentSubmissionsGet();
+    Route approveSubmissionPost();
+    Route submissionsReRunPost();
+
+    // UserController
+
+    Route repoPatch();
+    Route repoPatchAdmin();
+    Route repoHistoryAdminGet();
+}
