aa8eb1fd0595f8f5b952d29c3d465f62ae8be343
David Higueros <dav.higueros@gmail.com>
2024-12-19 17:56:20 +0000

    Created Overload of fetchRepoFromUrl that doesn't take a File directory, but creates it and returns it

--- src/main/java/edu/byu/cs/autograder/git/GitHelper.java
+++ src/main/java/edu/byu/cs/autograder/git/GitHelper.java
@@ -29,6 +29,7 @@ import java.io.IOException;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.UUID;
 
 public class GitHelper {
     private static final Logger LOGGER = LoggerFactory.getLogger(GitHelper.class);
@@ -89,6 +90,12 @@ public class GitHelper {
         gradingContext.observer().update("Fetching repo...");
 
         fetchRepoFromUrl(gradingContext.repoUrl(), intoDirectory);
+    }
+
+    public static File fetchRepoFromUrl(String repoUrl) throws GradingException {
+        File cloningDir = new File("./tmp" + UUID.randomUUID());
+        fetchRepoFromUrl(repoUrl, cloningDir);
+        return cloningDir;
     }
 
     //Method must be static due to the lack of GradingContext in cases where this method is required
