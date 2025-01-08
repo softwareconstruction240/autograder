5c73fe996f53ba54b87356437f826f0cb780607d
DallinFromEarth <112426674+DallinFromEarth@users.noreply.github.com>
2025-01-08 13:42:21 -0700

    restore configService changes from c3bef5b1

--- src/main/resources/frontend/src/services/configService.ts
+++ src/main/resources/frontend/src/services/configService.ts
@@ -1,79 +1,76 @@
-import { type Config, useAppConfigStore } from '@/stores/appConfig'
+import { type PrivateConfig, type PublicConfig, useConfigStore } from '@/stores/config'
 import {Phase, type RubricInfo, type RubricType} from '@/types/types'
-import { useAuthStore } from '@/stores/auth'
 import { ServerCommunicator } from '@/network/ServerCommunicator'
 import { ServerError } from '@/network/ServerError'
 
-export const getConfig = (): Promise<Config> => {
-  let endpoint = "/api"
-  if (useAuthStore().user?.role == 'ADMIN') {
-    endpoint += "/admin"
-  }
-  endpoint += "/config"
-
-  return ServerCommunicator.getRequest<Config>(endpoint)
+export const getPublicConfig = async ():Promise<PublicConfig> => {
+  return await ServerCommunicator.getRequest<PublicConfig>("/api/config")
 }
 
-export const setPenalties = (maxLateDaysPenalized: number,
+export const getAdminConfig = async ():Promise<PrivateConfig> => {
+  return await ServerCommunicator.getRequest<PrivateConfig>("/api/admin/config")
+}
+
+export const setPenalties = async (maxLateDaysPenalized: number,
                                    gitCommitPenalty: number,
                                    perDayLatePenalty: number,
                                    linesChangedPerCommit: number,
                                    clockForgivenessMinutes: number) => {
-  return doSetConfigItem("POST", '/api/admin/config/penalties', {
-    maxLateDaysPenalized,
-    gitCommitPenalty,
-    perDayLatePenalty,
-    linesChangedPerCommit,
-    clockForgivenessMinutes,
+  await doSetConfigItem("POST", '/api/admin/config/penalties', {
+    maxLateDaysPenalized: maxLateDaysPenalized,
+    gitCommitPenalty: gitCommitPenalty,
+    perDayLatePenalty: perDayLatePenalty,
+    linesChangedPerCommit: linesChangedPerCommit,
+    clockForgivenessMinutes: clockForgivenessMinutes
   })
 }
 
-export const setBanner = (message: String, link: String, color: String, expirationTimestamp: String): Promise<void> => {
-  return doSetConfigItem("POST", '/api/admin/config/banner', {
-    "bannerMessage": message,
-    "bannerLink": link,
-    "bannerColor": color,
-    "bannerExpiration": expirationTimestamp
+export const setBanner = async (message: String, link: String, color: String, expirationTimestamp: String): Promise<void> => {
+  await doSetConfigItem("POST", '/api/admin/config/banner', {
+      "bannerMessage": message,
+      "bannerLink": link,
+      "bannerColor": color,
+      "bannerExpiration": expirationTimestamp
     }
   );
 }
 
-export const setLivePhases = (phases: Array<Phase>): Promise<void> => {
-  return doSetConfigItem("POST", '/api/admin/config/phases', {"phases": phases});
+export const setLivePhases = async (phases: Array<Phase>): Promise<void> => {
+  await doSetConfigItem("POST", '/api/admin/config/phases', {"phases": phases});
 }
 
-export const setGraderShutdown = (shutdownTimestamp: string, shutdownWarningHours: number): Promise<void> => {
+export const setGraderShutdown = async (shutdownTimestamp: string, shutdownWarningHours: number): Promise<void> => {
   if (shutdownWarningHours < 0) shutdownWarningHours = 0
 
-  return doSetConfigItem("POST", "/api/admin/config/phases/shutdown", {
+  await doSetConfigItem("POST", "/api/admin/config/phases/shutdown", {
     "shutdownTimestamp": shutdownTimestamp,
     "shutdownWarningMilliseconds": Math.trunc(shutdownWarningHours * 60 * 60 * 1000) // convert to milliseconds
   })
 }
 
-export const setCanvasCourseIds = (): Promise<void> => {
-  return doSetConfigItem("GET", "/api/admin/config/courseIds", {});
+export const setCanvasCourseIds = async (): Promise<void> => {
+  await doSetConfigItem("GET", "/api/admin/config/courseIds", {});
 }
 
-const convertRubricInfoToObj = (rubricInfo: Map<Phase, Map<RubricType, RubricInfo>>): Record<Phase, Record<RubricType, RubricInfo>> => {
-    const obj = {} as Record<Phase, Record<string, RubricInfo>>;
-    rubricInfo.forEach((rubricTypeMap, phase) => {
-        obj[phase] = Object.fromEntries(rubricTypeMap.entries());
-    });
-    return obj;
+const convertRubricInfoToObj = (rubricInfo: Map<Phase, Map<RubricType, RubricInfo>>): object => {
+  const obj: any = {};
+  rubricInfo.forEach((rubricTypeMap, phase) => {
+    obj[phase] = Object.fromEntries(rubricTypeMap.entries());
+  });
+  return obj;
 }
 
-export const setCourseIds = (
-    courseNumber: number,
-    assignmentIds: Map<Phase, number>,
-    rubricInfo: Map<Phase, Map<RubricType, RubricInfo>>
+export const setCourseIds = async (
+  courseNumber: number,
+  assignmentIds: Map<Phase, number>,
+  rubricInfo: Map<Phase, Map<RubricType, RubricInfo>>
 ): Promise<void> => {
-    const body = {
-        "courseNumber": courseNumber,
-        "assignmentIds": Object.fromEntries(assignmentIds.entries()),
-        "rubricInfo": convertRubricInfoToObj(rubricInfo)
-    };
-    return doSetConfigItem("POST", "/api/admin/config/courseIds", body);
+  const body = {
+    "courseNumber": courseNumber,
+    "assignmentIds": Object.fromEntries(assignmentIds.entries()),
+    "rubricInfo": convertRubricInfoToObj(rubricInfo)
+  };
+  await doSetConfigItem("POST", "/api/admin/config/courseIds", body);
 }
 
 const doSetConfigItem = async (method: string, path: string, body: Object): Promise<void> => {
@@ -89,5 +86,5 @@ const doSetConfigItem = async (method: string, path: string, body: Object): Prom
     }
   }
 
-  await useAppConfigStore().updateConfig();
+  await useConfigStore().updateConfig();
 }
