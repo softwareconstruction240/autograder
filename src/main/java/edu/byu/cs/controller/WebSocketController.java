05b34720c77fce1a975631d661bf9d7e9c36454e
James Finlinson <finljam@byu.edu>
2025-01-23 16:28:37 -0700

    Refactor WebSocketController: split OnMessage into documented functions
    This refactor does not change any behavior, but merely improve the
    documentation and readability of the class.

--- src/main/java/edu/byu/cs/controller/WebSocketController.java
+++ src/main/java/edu/byu/cs/controller/WebSocketController.java
@@ -26,32 +26,67 @@ public class WebSocketController {
 
     @OnWebSocketMessage
     public void onMessage(Session session, String message) {
-        String netId;
-        try {
-            netId = JwtUtils.validateToken(message);
-        } catch (Exception e) {
-            LOGGER.warn("Exception thrown while validating token: ", e);
-
+        String netId = authenticateMessage(message);
+        if (netId == null) {
             sendError(session, "Invalid token");
             session.close();
             return;
         }
 
+        if(!isInQueue(netId)) {
+            sendError(session, "You are not in the queue");
+            session.close();
+            return;
+        }
+
+        if (TrafficController.hasSession(netId, session)) {
+            return;
+        }
+
+        // Register the session to receive updates,
+        // notify all queue members of the new queue state.
+        TrafficController.addSession(netId, session);
+        updateAllQueueMembers();
+    }
+
+    /**
+     * Extracts the token from the WebSocket message, authenticates it.
+     * When the token is valid, it returns the netId of the student it belongs to.
+     *
+     * @param message A WebSocket message
+     * @return The students verified netId, or null if the token is invalid or incomplete.
+     */
+    private String authenticateMessage(String message) {
         try {
-            if (!DaoService.getQueueDao().isAlreadyInQueue(netId)) {
-                sendError(session, "You are not in the queue");
-                session.close();
-                return;
-            }
+            return JwtUtils.validateToken(message);
+        } catch (Exception e) {
+            LOGGER.warn("Exception thrown while validating token: ", e);
+            return null;
+        }
+    }
+
+    /**
+     * Indicates whether the student is the pass-off queue.
+     *
+     * @param netId The student NetId to verify.
+     * @return A boolean indicating the result.
+     * @throws RuntimeException When a {@link DataAccessException} occurs.
+     */
+    private boolean isInQueue(String netId) {
+        try {
+            return DaoService.getQueueDao().isAlreadyInQueue(netId);
         } catch (DataAccessException e) {
             LOGGER.error("Error accessing queue", e);
             throw new RuntimeException("Error accessing queue", e);
         }
+    }
 
-        if (TrafficController.hasSession(netId, session))
-            return;
-
-        TrafficController.addSession(netId, session);
+    /**
+     * Updates everyone in the queue of their new state in the queue.
+     *
+     * @throws RuntimeException When a {@link DataAccessException} occurs.
+     */
+    private void updateAllQueueMembers() {
         try {
             TrafficController.broadcastQueueStatus();
         } catch (DataAccessException e) {
