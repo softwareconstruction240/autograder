656bac6d254f84e12c40a1a5bbfa27f83529d39c
James Finlinson <finljam@byu.edu>
2025-01-30 11:49:45 -0700

    Reduce commits out of bounds to warnings only

--- src/main/java/edu/byu/cs/autograder/git/CommitValidation/DefaultGitVerificationStrategy.java
+++ src/main/java/edu/byu/cs/autograder/git/CommitValidation/DefaultGitVerificationStrategy.java
@@ -44,12 +44,6 @@ public class DefaultGitVerificationStrategy implements CommitVerificationStrateg
                         insufficientDaysWithCommits,
                         String.format("Did not commit on enough days to pass off (%d/%d).", daysWithCommits, requiredDaysWithCommits)),
                 new CV(
-                        commitsByDay.commitsInFuture(),
-                        "Suspicious commit history. Some commits are authored after the hand in date."),
-                new CV(
-                        commitsByDay.commitsInPast(),
-                        "Suspicious commit history. Some commits are authored before the previous phase hash."),
-                new CV(
                         commitsByDay.commitsBackdated(),
                         "Suspicious commit history. Some commits have been backdated."),
                 new CV(
@@ -67,6 +61,12 @@ public class DefaultGitVerificationStrategy implements CommitVerificationStrateg
                         commitsByDay.commitTimestampsDuplicated(),
                         commitsByDay.getErroringCommitsSet("commitTimestampsDuplicatedSubsequentOnly"),
                         "Mistaken history manipulation. Multiple commits have the exact same timestamp. Likely, commits were pushed and amended and merged together."),
+                new CV(
+                        commitsByDay.commitsInFuture(),
+                        "Potentially accidental commit inclusion. Some commits are authored after the hand in date."),
+                new CV(
+                        commitsByDay.commitsInPast(),
+                        "Some commits excluded. Commits authored before the previous phase hash were not counted."),
         };
 
         // Preserve the first set of messages (preserve the original warnings about amending commits)
