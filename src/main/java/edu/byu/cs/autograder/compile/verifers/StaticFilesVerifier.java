b3770ae3dd31620fdbe51461f80478f7cae7cc1f
Maillman <awsome7mailman@gmail.com>
2025-01-31 21:21:59 +0000

    made filePath a class and reworded the warning

--- src/main/java/edu/byu/cs/autograder/compile/verifers/StaticFilesVerifier.java
+++ src/main/java/edu/byu/cs/autograder/compile/verifers/StaticFilesVerifier.java
@@ -9,15 +9,16 @@ import edu.byu.cs.autograder.compile.StudentCodeVerifier;
 import java.io.File;
 
 public class StaticFilesVerifier implements StudentCodeVerifier {
+    private static final String FILE_PATH = "server/src/main/resources/web";
 
     @Override
     public void verify(GradingContext context, StudentCodeReader reader) throws GradingException {
         if(context.phase()==Phase.Phase3){
-            String filePath = "server/src/main/resources/web";
-            File staticFiles = new File(context.stageRepo(), filePath);
+            File staticFiles = new File(context.stageRepo(), FILE_PATH);
             if(!staticFiles.exists() || !staticFiles.isDirectory()){
-                context.observer().notifyWarning("It looks like you won't pass the static files test. " +
-                        "Are the static files located in %s?".formatted(filePath));
+                context.observer().notifyWarning("Could not locate folder %s. ".formatted(FILE_PATH) +
+                        "This may lead to the autograder giving different results for the \"static files\" test." +
+                        "Ensure the files are in the specified location and committed and pushed to your repository.");
             }
         }
     }
