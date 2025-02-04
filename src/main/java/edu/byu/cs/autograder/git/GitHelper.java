b5ef7078942d23f2ec9d62996267bf4f446e4626
James Finlinson <finljam@byu.edu>
2025-02-04 03:53:05 -0700

    Revert "Install warning when hash is missing"
    This reverts commit 71592f11714a1694798eb0f579ee60ae45d37e2d.

--- src/main/java/edu/byu/cs/autograder/git/GitHelper.java
+++ src/main/java/edu/byu/cs/autograder/git/GitHelper.java
@@ -322,7 +322,6 @@ public class GitHelper {
         String latestCommitHash = null;
         Repository repo = git.getRepository();
         boolean hasCandidateSubmission = false;
-        boolean missingLatestHash = false;
 
         EffectiveTimestamp effectiveTimestamp;
         try (RevWalk revWalk = new RevWalk(repo)) {
@@ -335,10 +334,8 @@ public class GitHelper {
 
                 if (latestTimestamp == null || effectiveTimestamp.timestamp.isAfter(latestTimestamp)) {
                     latestTimestamp = effectiveTimestamp.timestamp;
-                    missingLatestHash = true;
                     if (effectiveTimestamp.commitExists) {
                         latestCommitHash = submission.headHash();
-                        missingLatestHash = false;
                     }
                 }
             }
@@ -349,11 +346,6 @@ public class GitHelper {
         }
         if (latestTimestamp == null) {
             throw new GradingException("After processing a non-empty set of passing submissions, our latestTimestamp timestamp is null.");
-        }
-        if (missingLatestHash) {
-            gradingContext.observer().notifyWarning("The latest commit hash could not be found, "
-                    + " even though passing & graded submissions exist."
-                    + "This is expected if you recently nuked your repository.");
         }
 
         return new CommitThreshold(latestTimestamp, latestCommitHash);
