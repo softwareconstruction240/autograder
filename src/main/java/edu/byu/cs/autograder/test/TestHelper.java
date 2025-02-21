1f69a64a39dd461729b46ec637ef6277692bcc18
19mdavenport <michaeldaven19@hotmail.com>
2025-02-21 22:32:36 +0000

    Revert "adjust test helper coverage on server module"
    This reverts commit 5f41a70b3afa66e7c0c8d5c5629861a79513911c.

--- src/main/java/edu/byu/cs/autograder/test/TestHelper.java
+++ src/main/java/edu/byu/cs/autograder/test/TestHelper.java
@@ -144,9 +144,8 @@ public class TestHelper {
                 .directory(compiledTests)
                 .command(runCommands);
 
-        String serverClassFiles = uberJar.getParentFile().getParentFile().getParent() + "server/target/classes";
         List<String> reportCommands = List.of("java", "-jar", jacocoCliJarPath, "report", "jacoco.exec",
-                "--classfiles", serverClassFiles, "--csv", "test-output/coverage.csv");
+                "--classfiles", uberJar.getParent() + "/classes", "--csv", "test-output/coverage.csv");
 
         ProcessBuilder reportProcessBuilder = new ProcessBuilder()
                 .command(reportCommands)
