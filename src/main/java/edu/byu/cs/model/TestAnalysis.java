049220b6a97150672aa5a077baf747e73cfb4704
Melvin Whitaker <awsome7mailman@gmail.com>
2025-02-27 12:00:07 -0700

    finished javadoc comment for TestAnalysis

--- src/main/java/edu/byu/cs/model/TestAnalysis.java
+++ src/main/java/edu/byu/cs/model/TestAnalysis.java
@@ -1,11 +1,12 @@
 package edu.byu.cs.model;
 
 /**
+ * Represents all the output from running the tests.
  *
- *
- * @param root a {@link TestNode} representing the results of the tests for an item
- * @param extraCredit a {@link TestNode} representing the results of the extra credit tests for an item
- * @param error
+ * @param root a {@link TestNode} representing the results of the tests
+ * @param extraCredit a {@link TestNode} representing the results of the extra credit tests
+ * @param error the error output from running the process of running the tests. This is what shows
+ *              when you click on the 'see details' button if the tests produced errors.
  */
 public record TestAnalysis(TestNode root, TestNode extraCredit, String error) {
 }
