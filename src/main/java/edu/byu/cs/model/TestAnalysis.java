6781899af97febad569a81145bc1eed1688382cf
Melvin Whitaker <awsome7mailman@gmail.com>
2025-02-26 20:01:34 -0700

    partially created javadoc comment for TestAnalysis

--- src/main/java/edu/byu/cs/model/TestAnalysis.java
+++ src/main/java/edu/byu/cs/model/TestAnalysis.java
@@ -1,4 +1,11 @@
 package edu.byu.cs.model;
 
+/**
+ *
+ *
+ * @param root a {@link TestNode} representing the results of the tests for an item
+ * @param extraCredit a {@link TestNode} representing the results of the extra credit tests for an item
+ * @param error
+ */
 public record TestAnalysis(TestNode root, TestNode extraCredit, String error) {
 }
