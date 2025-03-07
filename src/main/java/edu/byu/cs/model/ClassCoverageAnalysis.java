570fd1fdd82c6652b78fcba3d20b4a0134de8af3
Melvin Whitaker <awsome7mailman@gmail.com>
2025-03-07 14:30:21 -0700

    added javadoc comment for coverage analysis records

--- src/main/java/edu/byu/cs/model/ClassCoverageAnalysis.java
+++ src/main/java/edu/byu/cs/model/ClassCoverageAnalysis.java
@@ -1,4 +1,13 @@
 package edu.byu.cs.model;
 
+/**
+ * Represents the results for code coverage from running tests for a given class.
+ * Note that the requirement for code coverage can change between lines and branches.
+ *
+ * @param className the name of the class tested for code coverage
+ * @param packageName the name of the package to which the class belongs
+ * @param covered the number of lines/branches covered
+ * @param missed the number of lines/branches missed
+ */
 public record ClassCoverageAnalysis(String className, String packageName, int covered, int missed) {
 }
--- src/main/java/edu/byu/cs/model/CoverageAnalysis.java
+++ src/main/java/edu/byu/cs/model/CoverageAnalysis.java
@@ -2,6 +2,11 @@ package edu.byu.cs.model;
 
 import java.util.Collection;
 
+/**
+ * Represents the results for code coverage from running tests for a particular phase
+ *
+ * @param classAnalyses the collection of code coverage results for each class tested
+ */
 public record CoverageAnalysis(Collection<ClassCoverageAnalysis> classAnalyses) {
 
 }
