09b75d838eaf27007e6b52cfda8b0fdb25d5abfb
Melvin Whitaker <awsome7mailman@gmail.com>
2025-03-14 15:26:55 -0600

    added missing params in model module

--- src/main/java/edu/byu/cs/model/TestOutput.java
+++ src/main/java/edu/byu/cs/model/TestOutput.java
@@ -5,6 +5,7 @@ package edu.byu.cs.model;
  *
  * @param root a {@link TestNode} representing the results of the tests
  * @param extraCredit a {@link TestNode} representing the results of the extra credit tests
+ * @param coverage a {@link CoverageAnalysis} representing the results for code coverage from the tests
  * @param error the error output from running the process of running the tests. This is what shows
  *              when you click on the 'see details' button if the tests produced errors.
  */
