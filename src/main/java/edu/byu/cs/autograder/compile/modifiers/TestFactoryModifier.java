97b84563a11c44f890731225815019616e1dbf43
Melvin Whitaker <awsome7mailman@gmail.com>
2025-03-28 15:21:55 -0600

    clarified TestFactoryModifier javadoc comment

--- src/main/java/edu/byu/cs/autograder/compile/modifiers/TestFactoryModifier.java
+++ src/main/java/edu/byu/cs/autograder/compile/modifiers/TestFactoryModifier.java
@@ -8,8 +8,9 @@ import edu.byu.cs.util.FileUtils;
 import java.io.File;
 
 /**
- * Modifies the <code>getMessageTime()</code> method in the test factory for phase 6
- * so the WebSocketTests only wait 1 second
+ * Modifies the <code>getMessageTime()</code> method in the test factory in the student's
+ * code for phase 6 so the WebSocketTests only wait 1 second, but keeps the student's
+ * <code>getGsonBuilder()</code> implementation the same
  */
 public class TestFactoryModifier implements StudentCodeModifier {
 
