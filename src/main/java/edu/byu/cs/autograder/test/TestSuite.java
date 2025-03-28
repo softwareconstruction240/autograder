4cfc7f4982e8544738c1fa0821691bdf39657617
Melvin Whitaker <awsome7mailman@gmail.com>
2025-03-28 10:02:48 -0600

    added javadoc comments for TestSuite

--- src/main/java/edu/byu/cs/autograder/test/TestSuite.java
+++ src/main/java/edu/byu/cs/autograder/test/TestSuite.java
@@ -10,6 +10,19 @@ import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlText;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * A deserialization target for the output of running the Junit tests, since the output
+ * is printed into an XML file.
+ * <p>
+ * This is because printing the results out to the terminal may cause some tests to be
+ * interpreted as failing even though it didn't due to other printing from background
+ * threads intermingling with the Junit test output.
+ * </p>
+ * <p>
+ * The annotations in the class are so the XML serialization library will know where to
+ * look for the necessary information, since XML allows for more ambiguity than JSON.
+ * </p>
+ */
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class TestSuite {
     @JacksonXmlElementWrapper(useWrapping = false)
@@ -19,6 +32,9 @@ public class TestSuite {
         return testcase;
     }
 
+    /**
+     * The deserialization target for the output of running a single testcase
+     */
     @JsonIgnoreProperties(ignoreUnknown = true)
     public static class TestCase {
         @JacksonXmlProperty(isAttribute = true)
@@ -53,6 +69,9 @@ public class TestSuite {
         }
     }
 
+    /**
+     * The deserialization target for the output of a single category for a specific testcase
+     */
     @JsonIgnoreProperties(ignoreUnknown = true)
     public static class CData {
         @JacksonXmlCData
