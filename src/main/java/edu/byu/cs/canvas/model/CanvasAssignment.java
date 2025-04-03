1077ea621b16bb5a0c0ee476e7639a04fd72c963
Melvin Whitaker <awsome7mailman@gmail.com>
2025-04-03 16:25:22 -0600

    correct javadoc comment for CanvasAssignment.CanvasRubric

--- src/main/java/edu/byu/cs/canvas/model/CanvasAssignment.java
+++ src/main/java/edu/byu/cs/canvas/model/CanvasAssignment.java
@@ -17,15 +17,15 @@ public record CanvasAssignment(
         ZonedDateTime due_at,
         List<CanvasRubric> rubric
 ) {
-    /** A rubric item for a canvas assignment
+    /**
+     * Contains Canvas information for a rubric item. Note the difference between the {@code CanvasRubric} and
+     * the {@link CanvasRubricItem} that the AutoGrader creates to score a rubric item for a submission.
      *
      * @param id the id of the rubric item stored internally in Canvas (generally something like '_1234').
      *           This is stored in the rubric_config table in the autograder under the 'rubric_id' column.
-     *
      * @param points the amount of possible points for the rubric
-     * @param description a description of the rubric item (generally something like 'Passes passoff test cases').
-     *                    This is stored in the rubric_config table in the autograder under the 'criteria' column.
-     *
+     * @param description the name of the rubric item, for example, 'web api works'. The reason it's called
+     *                    description is because of the json sent from Canvas.
      */
     public record CanvasRubric(
             String id,
