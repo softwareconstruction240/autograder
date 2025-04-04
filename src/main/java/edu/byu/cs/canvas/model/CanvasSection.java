68a2fc79b686ff09b20d6236ecceb1910843acc4
Melvin Whitaker <awsome7mailman@gmail.com>
2025-04-04 14:09:36 -0600

    added javadoc comment for CanvasSection

--- src/main/java/edu/byu/cs/canvas/model/CanvasSection.java
+++ src/main/java/edu/byu/cs/canvas/model/CanvasSection.java
@@ -1,3 +1,9 @@
 package edu.byu.cs.canvas.model;
 
+/**
+ * Represents a section in Canvas
+ *
+ * @param id the id of the section stored internally in Canvas, e.g. 29055
+ * @param name the name of the section, e.g. CS 240-001: Adv Software Construction
+ */
 public record CanvasSection (Integer id, String name) {}
