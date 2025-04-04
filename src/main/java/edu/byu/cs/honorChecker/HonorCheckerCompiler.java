5cfbb527028f38f4e78779ea1c53f48fbd6b0299
19mdavenport <michaeldaven19@hotmail.com>
2025-04-04 22:05:30 +0000

    fix usages of refactored method

--- src/main/java/edu/byu/cs/honorChecker/HonorCheckerCompiler.java
+++ src/main/java/edu/byu/cs/honorChecker/HonorCheckerCompiler.java
@@ -3,6 +3,8 @@ package edu.byu.cs.honorChecker;
 import edu.byu.cs.canvas.CanvasException;
 import edu.byu.cs.canvas.CanvasService;
 import edu.byu.cs.canvas.model.CanvasSection;
+import edu.byu.cs.dataAccess.DaoService;
+import edu.byu.cs.dataAccess.DataAccessException;
 import edu.byu.cs.model.User;
 import edu.byu.cs.util.FileUtils;
 import org.eclipse.jgit.api.CloneCommand;
@@ -22,7 +24,7 @@ public class HonorCheckerCompiler {
      * @param sectionID the section ID
      * @return the path to the .zip file
      */
-    public static String compileSection(int sectionID) throws CanvasException {
+    public static String compileSection(int sectionID) throws CanvasException, DataAccessException {
         Optional<CanvasSection> canvasSection = Arrays.stream(CanvasService.getCanvasIntegration().getAllSections())
                 .filter(cs -> sectionID == cs.id()).findFirst();
         if (canvasSection.isEmpty()) throw new CanvasException("Could not find specified section");
@@ -31,21 +33,25 @@ public class HonorCheckerCompiler {
 
         FileUtils.createDirectory(tmpDir);
 
-        Collection<User> students;
+        Collection<String> studentNetIds;
         try {
-            students = CanvasService.getCanvasIntegration().getAllStudentsBySection(sectionID);
+            studentNetIds = CanvasService.getCanvasIntegration().getAllStudentNetIdsBySection(sectionID);
         } catch (CanvasException e) {
             throw new RuntimeException("Canvas Exception: " + e.getMessage());
         }
 
         try {
-            for (User student : students) {
-                if (student.firstName().equals("Test") && student.lastName().equals("Student")) continue;
+            for (String netId : studentNetIds) {
+                User student = DaoService.getUserDao().getUser(netId);
+                if (student == null || student.repoUrl() == null ||
+                        (student.firstName().equals("Test") && student.lastName().equals("Student"))) continue;
                 File repoPath = new File(tmpDir, String.join("_", student.firstName().replace(' ', '_'),
                         student.lastName().replace(' ', '_'), student.netId()));
 
+                User dbStudent = DaoService.getUserDao().getUser(student.netId());
+                if(dbStudent == null) continue;
                 CloneCommand cloneCommand = Git.cloneRepository()
-                        .setURI(student.repoUrl())
+                        .setURI(dbStudent.repoUrl())
                         .setDirectory(repoPath);
 
                 try {
@@ -77,7 +83,7 @@ public class HonorCheckerCompiler {
             }
 
             FileUtils.zipDirectory(tmpDir, zipFilePath);
-        } catch (RuntimeException e) {
+        } catch (RuntimeException | DataAccessException e) {
             FileUtils.removeDirectory(new File(tmpDir));
             new File(zipFilePath).delete();
             throw e;
