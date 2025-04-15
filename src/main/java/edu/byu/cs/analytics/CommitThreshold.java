f65b74db9b6b6ff0f7048103ec93c4c02bcbd801
Melvin Whitaker <awsome7mailman@gmail.com>
2025-04-15 14:39:25 -0600

    added javadoc comment for CommitThreshold

--- src/main/java/edu/byu/cs/analytics/CommitThreshold.java
+++ src/main/java/edu/byu/cs/analytics/CommitThreshold.java
@@ -5,6 +5,18 @@ import org.eclipse.jgit.annotations.Nullable;
 
 import java.time.Instant;
 
+/**
+ * Either the head or the tail commit threshold.
+ * <ul>
+ * <li>The head commit threshold is the latest commit in the phase
+ * (<STRONG>inclusively</STRONG> being graded during a submission).</li>
+ * <li>The tail commit threshold is the last commit already graded in a previous phase. This
+ * <STRONG>exclusively</STRONG> marks the end of the range of commits that will be considered.</li>
+ * </ul>
+ *
+ * @param timestamp the timestamp the commit was authored
+ * @param commitHash the hash of the commit that uniquely identifies it
+ */
 public record CommitThreshold(
         @NonNull Instant timestamp,
         @Nullable String commitHash
