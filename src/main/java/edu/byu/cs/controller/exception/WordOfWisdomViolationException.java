e664d1435a91a7e84850142547a4da1ac0d0e0d2
Melvin Whitaker <awsome7mailman@gmail.com>
2025-05-07 17:09:42 -0600

    clarify javadoc comment for WordOfWisdomViolationException

--- src/main/java/edu/byu/cs/controller/exception/WordOfWisdomViolationException.java
+++ src/main/java/edu/byu/cs/controller/exception/WordOfWisdomViolationException.java
@@ -1,7 +1,7 @@
 package edu.byu.cs.controller.exception;
 
 /**
- * Throws whenever a user tries claiming a repo url already claimed
+ * Throws whenever a user tries claiming a repo url already claimed by another user
  */
 public class WordOfWisdomViolationException extends Exception {
     private static final String shortAndStout = "yo im a teapot";
