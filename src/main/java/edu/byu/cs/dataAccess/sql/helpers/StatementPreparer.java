02d9e03a6567ae1e7a417ae90f7b06ff21690e47
Melvin Whitaker <awsome7mailman@gmail.com>
2025-05-28 11:50:51 -0600

    added javadoc comment for StatementPreparer

--- src/main/java/edu/byu/cs/dataAccess/sql/helpers/StatementPreparer.java
+++ src/main/java/edu/byu/cs/dataAccess/sql/helpers/StatementPreparer.java
@@ -3,6 +3,10 @@ package edu.byu.cs.dataAccess.sql.helpers;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+/**
+ * A {@link FunctionalInterface} that can modify the {@link PreparedStatement}
+ * before it is executed
+ */
 @FunctionalInterface
 public interface StatementPreparer {
     void prepare(PreparedStatement ps) throws SQLException;
