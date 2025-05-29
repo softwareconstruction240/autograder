0b7f0a33da37aeb2dda117643bbde600574c7326
mewilker <mewilk@byu.edu>
2025-05-29 09:17:38 -0600

    get the tests running using env variables

--- src/test/java/edu/byu/cs/dataAccess/SubmissionDaoTest.java
+++ src/test/java/edu/byu/cs/dataAccess/SubmissionDaoTest.java
@@ -1,13 +1,19 @@
 package edu.byu.cs.dataAccess;
 
+import edu.byu.cs.dataAccess.daoInterface.SubmissionDao;
 import edu.byu.cs.dataAccess.sql.SqlDb;
 import edu.byu.cs.dataAccess.sql.SubmissionSqlDao;
+import edu.byu.cs.dataAccess.sql.UserSqlDao;
 import edu.byu.cs.model.Phase;
 import edu.byu.cs.model.Submission;
+import edu.byu.cs.model.User;
+import edu.byu.cs.properties.ApplicationProperties;
+
 import org.junit.jupiter.api.*;
 
 import java.time.Instant;
 import java.util.Random;
+import java.util.Properties;
 
 class SubmissionDaoTest {
     static Random random;
@@ -15,18 +21,32 @@ class SubmissionDaoTest {
 
     Submission firstSubmission;
 
-
+    /**
+     * In order to successfully run this test on your machine, you're going to need some environment variables
+     * 
+     * 
+    */
     @BeforeAll
     static void prepareDatabase() throws DataAccessException {
         random = new Random();
+        Properties props = new Properties();
+        props.setProperty("db-host", System.getenv("DB_HOST"));
+        props.setProperty("db-port", System.getenv("DB_PORT"));
+        props.setProperty("db-name", System.getenv("DB_NAME"));
+        props.setProperty("db-user", System.getenv("DB_USER"));
+        props.setProperty("db-pass", System.getenv("DB_PASS"));
+        ApplicationProperties.loadProperties(props);
         SqlDb.setUpDb();
     }
 
     @BeforeEach
     void setup() {
         dao = new SubmissionSqlDao();
-
-        firstSubmission = generateSubmission();
+        int cosmo_id = generateID();
+        //Submission table has a foreign key constraint, so we need to generate a user as well
+        User cosmo = generateStudentUser(cosmo_id);
+        firstSubmission = generateSubmission(cosmo_id);
+        Assertions.assertDoesNotThrow(()-> new UserSqlDao().insertUser(cosmo));
         Assertions.assertDoesNotThrow(() -> dao.insertSubmission(firstSubmission));
     }
 
@@ -46,23 +66,41 @@ class SubmissionDaoTest {
     void getSubmissionsForPhase() {
     }
 
-    private Submission generateSubmission() {
-        int cosmo_id = random.nextInt();
+    private User generateStudentUser(int id){
+        return new User(
+                "cosmo_" + id,
+                generateID(),
+                "Cosmo",
+                "Cougar",
+                generateRepo(id),
+                User.Role.STUDENT
+        );
+    }
+
+    private Submission generateSubmission(int id) {
         return new Submission(
-                "cosmo_" + cosmo_id,
-                "https://github.com/cosmo_%s/chess".formatted(cosmo_id),
+                "cosmo_" + id,
+                generateRepo(id),
                 "fc80e76ee5bfa331840bc75d1a5efec2cc7a874c",
                 Instant.now().minusSeconds(60L * random.nextInt(1, 100)),
                 Phase.Phase0,
                 true,
                 1.618f,
                 3.1415f,
-                "This is only a testing submission for Cosmo Cougar (#%s).".formatted(cosmo_id),
+                "This is only a testing submission for Cosmo Cougar (#%s).".formatted(id),
                 null,
                 true,
                 Submission.VerifiedStatus.ApprovedManually,
                 null, null,
                 new Submission.ScoreVerification(100.1f, "cosmo_boss", Instant.now(),10)
         );
+    }
+
+    private int generateID(){
+        return random.nextInt();
+    }
+
+    private String generateRepo(int id){
+        return "https://github.com/cosmo_%s/chess".formatted(id);
     }
 }
