52b00bd262ee0a264b6746600ed937f9f9423276
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-06-16 22:45:04 +0000

    Changed to implement new abstract class to reduce duplication

--- src/main/java/edu/byu/cs/autograder/compile/modifiers/PassoffJarModifier.java
+++ src/main/java/edu/byu/cs/autograder/compile/modifiers/PassoffJarModifier.java
@@ -1,20 +1,14 @@
 package edu.byu.cs.autograder.compile.modifiers;
 
 import edu.byu.cs.autograder.GradingContext;
-import edu.byu.cs.autograder.compile.StudentCodeModifier;
-import edu.byu.cs.util.FileUtils;
-
-import java.io.File;
 
 /**
  * Replaces the passoff-dependencies jar in the staged repo with the passoff-dependencies jar
  * stored locally. This ensures the tests use the most current version of the jar.
  */
-public class PassoffJarModifier implements StudentCodeModifier {
+public class PassoffJarModifier extends DirectFileModifier {
     @Override
     public void modify(GradingContext context) {
-        File oldJar = new File(context.stageRepo(), "server/lib/passoff-dependencies.jar");
-        File newJar = new File(context.phasesPath(), "libs/passoff-dependencies.jar");
-        FileUtils.copyFile(oldJar, newJar);
+        replaceFile(context, "server/lib/passoff-dependencies.jar", "libs/passoff-dependencies.jar");
     }
 }
--- src/main/java/edu/byu/cs/autograder/compile/modifiers/PomModifier.java
+++ src/main/java/edu/byu/cs/autograder/compile/modifiers/PomModifier.java
@@ -1,35 +1,19 @@
 package edu.byu.cs.autograder.compile.modifiers;
 
 import edu.byu.cs.autograder.GradingContext;
-import edu.byu.cs.autograder.compile.StudentCodeModifier;
-import edu.byu.cs.util.FileUtils;
-
-import java.io.File;
 
 /**
  * Replaces the pom files in the staged repo with the pom files stored locally.
  * This ensures that the pom files haven't been modified or deleted problematically
  * and that no external dependencies have been installed.
  */
-public class PomModifier implements StudentCodeModifier {
+public class PomModifier extends DirectFileModifier {
     @Override
     public void modify(GradingContext context) {
-        File oldRootPom = new File(context.stageRepo(), "pom.xml");
-        File oldServerPom = new File(context.stageRepo(), "server/pom.xml");
-        File oldClientPom = new File(context.stageRepo(), "client/pom.xml");
-        File oldSharedPom = new File(context.stageRepo(), "shared/pom.xml");
-        File oldAssembly = new File(context.stageRepo(), "test-dependencies-assembly.xml");
-
-        File newRootPom = new File(context.phasesPath(), "pom/pom.xml");
-        File newServerPom = new File(context.phasesPath(), "pom/server/pom.xml");
-        File newClientPom = new File(context.phasesPath(), "pom/client/pom.xml");
-        File newSharedPom = new File(context.phasesPath(), "pom/shared/pom.xml");
-        File newAssembly = new File(context.phasesPath(), "pom/test-dependencies-assembly.xml");
-
-        FileUtils.copyFile(oldRootPom, newRootPom);
-        FileUtils.copyFile(oldServerPom, newServerPom);
-        FileUtils.copyFile(oldClientPom, newClientPom);
-        FileUtils.copyFile(oldSharedPom, newSharedPom);
-        FileUtils.copyFile(oldAssembly, newAssembly);
+        replaceFile(context, "pom.xml", "pom/pom.xml");
+        replaceFile(context, "server/pom.xml", "pom/server/pom.xml");
+        replaceFile(context, "client/pom.xml", "pom/client/pom.xml");
+        replaceFile(context, "shared/pom.xml", "pom/shared/pom.xml");
+        replaceFile(context, "test-dependencies-assembly.xml", "pom/test-dependencies-assembly.xml");
     }
 }
