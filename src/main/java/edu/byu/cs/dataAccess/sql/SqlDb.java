8c8acab7c96bf971a748c32fc909f3652bfee93f
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-06-16 23:08:40 +0000

    Made table creation run on a loop

--- src/main/java/edu/byu/cs/dataAccess/sql/SqlDb.java
+++ src/main/java/edu/byu/cs/dataAccess/sql/SqlDb.java
@@ -4,6 +4,7 @@ import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import edu.byu.cs.dataAccess.DataAccessException;
 import edu.byu.cs.properties.ApplicationProperties;
+import org.intellij.lang.annotations.Language;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -35,6 +36,85 @@ public class SqlDb {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SqlDb.class);
 
+    @Language("SQL")
+    private static final String[] tableStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS `user` (
+                `net_id` varchar(20) NOT NULL,
+                `canvas_user_id` int NOT NULL,
+                `first_name` varchar(50) NOT NULL,
+                `last_name` varchar(50) NOT NULL,
+                `repo_url` varchar(200),
+                `role` varchar(15) NOT NULL,
+                PRIMARY KEY (`net_id`)
+            )
+            """,
+            """
+            CREATE TABLE IF NOT EXISTS `submission` (
+                `id` INT NOT NULL AUTO_INCREMENT,
+                `net_id` VARCHAR(20) NOT NULL,
+                `repo_url` VARCHAR(200) NOT NULL,
+                `head_hash` VARCHAR(40) NOT NULL,
+                `timestamp` DATETIME NOT NULL,
+                `phase` VARCHAR(9) NOT NULL,
+                `passed` BOOL NOT NULL,
+                `score` FLOAT NOT NULL,
+                `raw_score` FLOAT NOT NULL,
+                `notes` TEXT,
+                `rubric` JSON,
+                `verified_status` VARCHAR(30),
+                `commit_context` JSON,
+                `commit_result` JSON,
+                `verification` JSON,
+                `admin` BOOL NOT NULL,
+                PRIMARY KEY (`id`),
+                INDEX sort_index (`net_id`,`phase`,`passed`,`score`,`timestamp`),
+                CONSTRAINT `net_id`
+                    FOREIGN KEY (`net_id`)
+                    REFERENCES `user` (`net_id`)
+                    ON DELETE CASCADE
+                    ON UPDATE CASCADE
+            )
+            """,
+            """
+            CREATE TABLE IF NOT EXISTS `queue` (
+                `net_id` VARCHAR(20) NOT NULL,
+                `phase` VARCHAR(9) NOT NULL,
+                `time_added` DATETIME NOT NULL,
+                `started` BOOL,
+                PRIMARY KEY (`net_id`)
+            )
+            """,
+            """
+            CREATE TABLE IF NOT EXISTS `rubric_config` (
+                `phase` VARCHAR(9) NOT NULL,
+                `type` VARCHAR(15) NOT NULL,
+                `category` TEXT NOT NULL,
+                `criteria` TEXT NOT NULL,
+                `points` INT NOT NULL,
+                `rubric_id` VARCHAR(15),
+                PRIMARY KEY (`phase`, `type`)
+            )
+            """,
+            """
+            CREATE TABLE IF NOT EXISTS `configuration` (
+                `config_key` VARCHAR(50) NOT NULL,
+                `value` TEXT NOT NULL,
+                PRIMARY KEY (`config_key`)
+            )
+            """,
+            """
+            CREATE TABLE IF NOT EXISTS `repo_update` (
+                `timestamp` TIMESTAMP NOT NULL,
+                `net_id` VARCHAR(255) NOT NULL,
+                `repo_url` VARCHAR(2048) NOT NULL,
+                `admin_update` BOOLEAN NOT NULL,
+                `admin_net_id` VARCHAR(255),
+                PRIMARY KEY (`timestamp`)
+            )
+            """
+    };
+
     private static HikariDataSource dataSource;
 
     public static void setUpDb() throws DataAccessException {
@@ -44,92 +124,11 @@ public class SqlDb {
             stmt.executeUpdate("CREATE DATABASE IF NOT EXISTS " + DB_NAME);
             connection.setCatalog(DB_NAME);
 
-            try (Statement createUserTableStatement = connection.createStatement()) {
-                createUserTableStatement.executeUpdate("""
-                        CREATE TABLE IF NOT EXISTS `user` (
-                          `net_id` varchar(20) NOT NULL,
-                          `canvas_user_id` int NOT NULL,
-                          `first_name` varchar(50) NOT NULL,
-                          `last_name` varchar(50) NOT NULL,
-                          `repo_url` varchar(200),
-                          `role` varchar(15) NOT NULL,
-                          PRIMARY KEY (`net_id`)
-                        ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
-                        """);
-            }
-            try (Statement createSubmissionTableStatement = connection.createStatement()) {
-                createSubmissionTableStatement.executeUpdate("""
-                        CREATE TABLE IF NOT EXISTS `submission` (
-                                `id` INT NOT NULL AUTO_INCREMENT,
-                                `net_id` VARCHAR(20) NOT NULL,
-                                `repo_url` VARCHAR(200) NOT NULL,
-                                `head_hash` VARCHAR(40) NOT NULL,
-                                `timestamp` DATETIME NOT NULL,
-                                `phase` VARCHAR(9) NOT NULL,
-                                `passed` BOOL NOT NULL,
-                                `score` FLOAT NOT NULL,
-                                `raw_score` FLOAT NOT NULL,
-                                `notes` TEXT,
-                                `rubric` JSON,
-                                `verified_status` VARCHAR(30),
-                                `commit_context` JSON,
-                                `commit_result` JSON,
-                                `verification` JSON,
-                                `admin` BOOL NOT NULL,
-                                PRIMARY KEY (`id`),
-                                INDEX sort_index (`net_id`,`phase`,`passed`,`score`,`timestamp`),
-                                CONSTRAINT `net_id`
-                                    FOREIGN KEY (`net_id`)
-                                    REFERENCES `user` (`net_id`)
-                                    ON DELETE CASCADE
-                                    ON UPDATE CASCADE
-                            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
-                        """);
-            }
-            try (Statement createQueueTableStatement = connection.createStatement()) {
-                createQueueTableStatement.executeUpdate("""
-                        CREATE TABLE IF NOT EXISTS `queue` (
-                                `net_id` VARCHAR(20) NOT NULL,
-                                `phase` VARCHAR(9) NOT NULL,
-                                `time_added` DATETIME NOT NULL,
-                                `started` BOOL,
-                                PRIMARY KEY (`net_id`)
-                            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
-                        """);
-            }
-            try (Statement createRubricConfigTableStatement = connection.createStatement()) {
-                createRubricConfigTableStatement.executeUpdate("""
-                        CREATE TABLE IF NOT EXISTS `rubric_config` (
-                                `phase` VARCHAR(9) NOT NULL,
-                                `type` VARCHAR(15) NOT NULL,
-                                `category` TEXT NOT NULL,
-                                `criteria` TEXT NOT NULL,
-                                `points` INT NOT NULL,
-                                `rubric_id` VARCHAR(15),
-                                PRIMARY KEY (`phase`, `type`)
-                            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
-                        """);
-            }
-            try (Statement createConfigurationTableStatement = connection.createStatement()) {
-                createConfigurationTableStatement.executeUpdate("""
-                        CREATE TABLE IF NOT EXISTS `configuration` (
-                                `config_key` VARCHAR(50) NOT NULL,
-                                `value` TEXT NOT NULL,
-                                PRIMARY KEY (`config_key`)
-                            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
-                        """);
-            }
-            try (Statement createConfigurationTableStatement = connection.createStatement()) {
-                createConfigurationTableStatement.executeUpdate("""
-                        CREATE TABLE IF NOT EXISTS `repo_update` (
-                               `timestamp` TIMESTAMP NOT NULL,
-                               `net_id` VARCHAR(255) NOT NULL,
-                               `repo_url` VARCHAR(2048) NOT NULL,
-                               `admin_update` BOOLEAN NOT NULL,
-                               `admin_net_id` VARCHAR(255),
-                               PRIMARY KEY (`timestamp`)
-                            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
-                        """);
+            for (String createTable : tableStatements) {
+                try (Statement createTableStatement = connection.createStatement()) {
+                    createTableStatement.executeUpdate(createTable
+                            + " ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci");
+                }
             }
             dataSource.setCatalog(DB_NAME);
         } catch (SQLException e) {
