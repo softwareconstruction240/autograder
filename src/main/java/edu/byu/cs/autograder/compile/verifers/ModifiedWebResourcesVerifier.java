2d60abd2f37ced6501d5a44fbe2c2021edd3b15b
Maillman <awsome7mailman@gmail.com>
2025-07-02 00:34:58 +0000

    created empty ModifiedWebResourcesVerifier class

---
+++ src/main/java/edu/byu/cs/autograder/compile/verifers/ModifiedWebResourcesVerifier.java
@@ -0,0 +1,13 @@
+package edu.byu.cs.autograder.compile.verifers;
+
+import edu.byu.cs.autograder.GradingContext;
+import edu.byu.cs.autograder.GradingException;
+import edu.byu.cs.autograder.compile.StudentCodeReader;
+import edu.byu.cs.autograder.compile.StudentCodeVerifier;
+
+public class ModifiedWebResourcesVerifier implements StudentCodeVerifier {
+    @Override
+    public void verify(GradingContext context, StudentCodeReader reader) throws GradingException {
+
+    }
+}
