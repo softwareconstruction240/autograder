0a27f8c94d174c6625176cbe5f17d3cc08e37a04
mewilker <mewilk@byu.edu>
2025-07-17 15:37:18 -0600

    added at least one test for every user dao method

--- src/test/java/edu/byu/cs/dataAccess/base/UserDaoTest.java
+++ src/test/java/edu/byu/cs/dataAccess/base/UserDaoTest.java
@@ -29,7 +29,7 @@ public abstract class UserDaoTest {
         noRepoStudent = new User(
                 DaoTestUtils.generateNetID(DaoTestUtils.generateID()),
                 DaoTestUtils.generateID(),
-                "Rookie D",
+                "Rookie Dee",
                 "Noob",
                 null,
                 User.Role.STUDENT
@@ -45,8 +45,9 @@ public abstract class UserDaoTest {
     }
 
     @BeforeEach
-    void getDao(){
+    void getDao() throws DataAccessException{
         dao = newUserDao();
+        clearUsers();
     }
 
     private static HashSet<User> provideTestUsers(){
@@ -60,7 +61,6 @@ public abstract class UserDaoTest {
     @ParameterizedTest
     @MethodSource("provideTestUsers")
     void insertUserTest(User user) throws DataAccessException {
-        clearUsers();
         Assertions.assertDoesNotThrow(() -> dao.insertUser(user));
         User obtained = dao.getUser(user.netId());
         Assertions.assertEquals(user, obtained);
@@ -68,10 +68,122 @@ public abstract class UserDaoTest {
 
     @Test
     void insertUserTwiceFails() throws DataAccessException {
-        clearUsers();
         Assertions.assertDoesNotThrow(() -> {dao.insertUser(student);});
-
+        Assertions.assertThrows(DataAccessException.class, () -> dao.insertUser(student));
     }
 
-    
+    @ParameterizedTest
+    @MethodSource("provideTestUsers")
+    void getUser(User user) throws DataAccessException {
+        addAllTestUsers();
+        User obtained = dao.getUser(user.netId());
+        Assertions.assertEquals(user, obtained);
+    }
+
+    @Test
+    void getNoUsers() throws DataAccessException {
+        User obtained = dao.getUser("Does_Not_Exist");
+        Assertions.assertNull(obtained);
+        addAllTestUsers();
+        obtained = dao.getUser("Still_Not_Here");
+        Assertions.assertNull(obtained);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideTestUsers")
+    void setFirstName(User user) throws DataAccessException{
+        dao.insertUser(user);
+        String changed = "Changed";
+        User expected = new User(
+                user.netId(),
+                user.canvasUserId(),
+                changed,
+                user.lastName(),
+                user.repoUrl(),
+                user.role()
+        );
+        Assertions.assertDoesNotThrow(() -> dao.setFirstName(user.netId(), changed));
+        User obtained = dao.getUser(expected.netId());
+        Assertions.assertEquals(expected, obtained);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideTestUsers")
+    void setLastName(User user) throws DataAccessException{
+        dao.insertUser(user);
+        String changed = "Changed";
+        User expected = new User(
+                user.netId(),
+                user.canvasUserId(),
+                user.firstName(),
+                changed,
+                user.repoUrl(),
+                user.role()
+        );
+        Assertions.assertDoesNotThrow(() -> dao.setLastName(user.netId(), changed));
+        User obtained = dao.getUser(expected.netId());
+        Assertions.assertEquals(expected, obtained);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideTestUsers")
+    void setRepoUrl(User user) throws DataAccessException{
+        dao.insertUser(user);
+        String changed = DaoTestUtils.generateRepo(0);
+        User expected = new User(
+                user.netId(),
+                user.canvasUserId(),
+                user.firstName(),
+                user.lastName(),
+                changed,
+                user.role()
+        );
+        Assertions.assertDoesNotThrow(() -> dao.setRepoUrl(user.netId(), changed));
+        User obtained = dao.getUser(expected.netId());
+        Assertions.assertEquals(expected, obtained);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideTestUsers")
+    void setRole(User user) throws DataAccessException {
+        dao.insertUser(user);
+        for (User.Role role : User.Role.values()){
+            User expected = new User(
+                    user.netId(),
+                    user.canvasUserId(),
+                    user.firstName(),
+                    user.lastName(),
+                    user.repoUrl(),
+                    role
+            );
+            Assertions.assertDoesNotThrow(() -> dao.setRole(user.netId(), role));
+            User obtained = dao.getUser(expected.netId());
+            Assertions.assertEquals(expected, obtained);
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideTestUsers")
+    void setCanvasUserId(User user) throws DataAccessException {
+        dao.insertUser(user);
+        User expected = new User(
+                user.netId(),
+                0,
+                user.firstName(),
+                user.lastName(),
+                user.repoUrl(),
+                user.role()
+        );
+        Assertions.assertDoesNotThrow(() -> dao.setCanvasUserId(user.netId(), 0));
+        User obtained = dao.getUser(expected.netId());
+        Assertions.assertEquals(expected, obtained);
+    }
+
+    private void addAllTestUsers() throws DataAccessException{
+        for (User u : provideTestUsers()){
+            dao.insertUser(u);
+        }
+    }
+
+
 }
