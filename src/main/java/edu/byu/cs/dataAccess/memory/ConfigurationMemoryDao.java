a86a01819c1798e873066119eb2b7c240d80f392
mewilker <mewilk@byu.edu>
2025-09-06 12:52:50 -0600

    change ConfigurationMemoryDao to match behavior of sql dao with null key

--- src/main/java/edu/byu/cs/dataAccess/memory/ConfigurationMemoryDao.java
+++ src/main/java/edu/byu/cs/dataAccess/memory/ConfigurationMemoryDao.java
@@ -1,5 +1,6 @@
 package edu.byu.cs.dataAccess.memory;
 
+import edu.byu.cs.dataAccess.DataAccessException;
 import edu.byu.cs.dataAccess.daoInterface.ConfigurationDao;
 
 import java.util.HashMap;
@@ -10,7 +11,10 @@ public class ConfigurationMemoryDao implements ConfigurationDao {
     private final Map<Configuration, Object> configuration = new HashMap<>();
 
     @Override
-    public <T> void setConfiguration(Configuration key, T value, Class<T> type) {
+    public <T> void setConfiguration(Configuration key, T value, Class<T> type) throws DataAccessException {
+        if (key == null){
+            throw new DataAccessException("Cannot set a configuration with a null key");
+        }
         configuration.put(key, value);
     }
 
