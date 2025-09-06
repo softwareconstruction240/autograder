b290b7fe53957bb369d6ec8d03ff88ea6ca04456
mewilker <mewilk@byu.edu>
2025-09-06 14:18:39 -0600

    fix ConfigurationMemoryDao to match sql behavior with setting an invalid type

--- src/main/java/edu/byu/cs/dataAccess/memory/ConfigurationMemoryDao.java
+++ src/main/java/edu/byu/cs/dataAccess/memory/ConfigurationMemoryDao.java
@@ -3,6 +3,7 @@ package edu.byu.cs.dataAccess.memory;
 import edu.byu.cs.dataAccess.DataAccessException;
 import edu.byu.cs.dataAccess.daoInterface.ConfigurationDao;
 
+import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,8 +13,13 @@ public class ConfigurationMemoryDao implements ConfigurationDao {
 
     @Override
     public <T> void setConfiguration(Configuration key, T value, Class<T> type) throws DataAccessException {
-        if (key == null){
-            throw new DataAccessException("Cannot set a configuration with a null key");
+        if (key == null || type == null){
+            throw new DataAccessException("Cannot set a configuration with a null key or type");
+        }
+        switch(type.getName()){
+            case  "java.lang.Boolean", "java.lang.String", "java.lang.Integer", "java.time.Instant",
+                  "java.lang.Float" -> configuration.put(key, value);
+            case null, default -> throw new DataAccessException("Invalid type:" + type.getName());
         }
         configuration.put(key, value);
     }
