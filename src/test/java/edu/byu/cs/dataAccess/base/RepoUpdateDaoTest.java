66d2fd078f59ec4a0721ed2b863a3a5ea3b7bf38
mewilker <mewilk@byu.edu>
2025-09-16 19:51:00 -0600

    get repo updates with url test and refactoring for clarity

--- src/test/java/edu/byu/cs/dataAccess/base/RepoUpdateDaoTest.java
+++ src/test/java/edu/byu/cs/dataAccess/base/RepoUpdateDaoTest.java
@@ -1,5 +1,6 @@
 package edu.byu.cs.dataAccess.base;
 
+import edu.byu.cs.autograder.test.PreviousPhasePassoffTestGrader;
 import edu.byu.cs.dataAccess.DataAccessException;
 import edu.byu.cs.dataAccess.daoInterface.RepoUpdateDao;
 import edu.byu.cs.model.RepoUpdate;
@@ -20,6 +21,7 @@ public abstract class RepoUpdateDaoTest {
     private static Random random = new Random();
 
     String netId = "cosmo_cougar";
+    String repoLink = "https://github.com/" + netId + "/chess";
 
     @BeforeEach
     public void setup() throws DataAccessException{
@@ -29,7 +31,7 @@ public abstract class RepoUpdateDaoTest {
 
     @Test
     public void insertAndGetOneValidRepoUpdate () throws DataAccessException{
-        RepoUpdate update = generateRepoUpdate(netId);
+        RepoUpdate update = generateRepoUpdate(netId, repoLink);
         dao.insertUpdate(update);
         Collection<RepoUpdate> updates = dao.getUpdatesForUser(netId);
         Assertions.assertEquals(1, updates.size());
@@ -44,16 +46,16 @@ public abstract class RepoUpdateDaoTest {
     @Test
     public void getRepoUpdatesForValidUser() throws DataAccessException{
         Collection<RepoUpdate> updates = generateManyRepoUpdates(3);
-        for (RepoUpdate update : updates){
-            dao.insertUpdate(update);
-        }
-        Collection<RepoUpdate> validUserUpdates = generateManyRepoUpdates(3, netId);
-        for (RepoUpdate update : updates){
-            dao.insertUpdate(update);
-        }
+        insertAllRepoUpdates(updates);
+
+        Collection<RepoUpdate> validUserUpdates = generateManyRepoUpdates(3, netId, null);
+        insertAllRepoUpdates(validUserUpdates);
+
         Collection<RepoUpdate> obtainedUpdates = dao.getUpdatesForUser(netId);
+
         Assertions.assertEquals(validUserUpdates.size(), obtainedUpdates.size());
         Assertions.assertTrue(obtainedUpdates.containsAll(validUserUpdates));
+
         for (RepoUpdate update : updates) {
             Assertions.assertFalse(obtainedUpdates.contains(update));
         }
@@ -62,18 +64,30 @@ public abstract class RepoUpdateDaoTest {
     @Test
     public void getNoRepoUpdatesForInvalidUser() throws DataAccessException{
         Collection<RepoUpdate> updates = generateManyRepoUpdates(3);
-        for (RepoUpdate update : updates){
-            dao.insertUpdate(update);
-        }
+        insertAllRepoUpdates(updates);
+
         Collection<RepoUpdate> obtainedUpdates = dao.getUpdatesForUser(null);
         Assertions.assertTrue(obtainedUpdates.isEmpty());
+
         obtainedUpdates = dao.getUpdatesForUser(netId);
         Assertions.assertTrue(obtainedUpdates.isEmpty());
     }
 
     @Test
-    public void getRepoUpdatesForValidRepoUrl(){
+    public void getRepoUpdatesForValidRepoUrl() throws DataAccessException{
+        Collection<RepoUpdate> updates = generateManyRepoUpdates(3);
+        insertAllRepoUpdates(updates);
 
+        Collection<RepoUpdate> updatesWithSameRepo = generateManyRepoUpdates(3, null, repoLink);
+        insertAllRepoUpdates(updatesWithSameRepo);
+
+        Collection<RepoUpdate> obtainedUpdates = dao.getUpdatesForRepo(repoLink);
+        Assertions.assertEquals(updatesWithSameRepo.size(), obtainedUpdates.size());
+        Assertions.assertTrue(obtainedUpdates.containsAll(updatesWithSameRepo));
+
+        for (RepoUpdate update : updates){
+            Assertions.assertFalse(obtainedUpdates.contains(update));
+        }
     }
 
     @Test
@@ -81,30 +95,41 @@ public abstract class RepoUpdateDaoTest {
 
     }
 
-    private RepoUpdate generateRepoUpdate(String netId){
+    private void insertAllRepoUpdates(Collection<RepoUpdate> updates) throws DataAccessException{
+        for (RepoUpdate update : updates){
+            dao.insertUpdate(update);
+        }
+    }
+
+    private RepoUpdate generateRepoUpdate(String netId, String repoLink){
         boolean admin = random.nextBoolean();
         return new RepoUpdate(
-                Instant.now().truncatedTo(ChronoUnit.SECONDS),
+                //FIXME primary key for this table is time, and with this code there's roughly a 1/365 chance for a collision
+                Instant.now().truncatedTo(ChronoUnit.SECONDS).minus(random.nextInt(0,365), ChronoUnit.DAYS),
                 netId,
-                "https://github.com/" + netId + "/chess",
+                repoLink,
                 admin,
                 admin ? "Cosmo_Boss" : null
         );
     }
 
     private HashSet<RepoUpdate> generateManyRepoUpdates(int size){
-        var updates = new HashSet<RepoUpdate>();
-        for (int i = 0; i < size; i++){
-            int id = random.nextInt();
-            updates.add(generateRepoUpdate("cosmo_"+id));
-        }
-        return updates;
+        return generateManyRepoUpdates(size, null, null);
     }
 
-    private HashSet<RepoUpdate> generateManyRepoUpdates(int size, String netId){
+    /**
+     *
+     * @param size
+     * @param netId null if random is desired
+     * @param repoLink null if it should be linked to netId
+     * @return
+     */
+    private HashSet<RepoUpdate> generateManyRepoUpdates(int size, String netId, String repoLink){
+        String user = netId == null ? "cosmo_" + random.nextInt() : netId;
+        String url = repoLink == null ? "https://github.com/" + user + "/chess" : repoLink;
         var updates = new HashSet<RepoUpdate>();
         for (int i = 0; i < size; i++){
-            updates.add(generateRepoUpdate(netId));
+            updates.add(generateRepoUpdate(user, url));
         }
         return updates;
     }
