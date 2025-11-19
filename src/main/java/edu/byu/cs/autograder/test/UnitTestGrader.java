adfe91a3b3ba325dc080c36ab1689fb80ac1611d
mewilker <mewilk@byu.edu>
2025-11-19 01:37:35 +0000

    score unit tests based on coverage rather than number failing

--- src/main/java/edu/byu/cs/autograder/test/UnitTestGrader.java
+++ src/main/java/edu/byu/cs/autograder/test/UnitTestGrader.java
@@ -1,15 +1,18 @@
 package edu.byu.cs.autograder.test;
 
-import edu.byu.cs.autograder.GradingContext;
-import edu.byu.cs.autograder.GradingException;
-import edu.byu.cs.model.Rubric;
-import edu.byu.cs.model.TestOutput;
-import edu.byu.cs.model.TestNode;
-import edu.byu.cs.util.PhaseUtils;
-
 import java.io.File;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.regex.Pattern;
+
+import edu.byu.cs.autograder.GradingContext;
+import edu.byu.cs.autograder.GradingException;
+import edu.byu.cs.model.ClassCoverageAnalysis;
+import edu.byu.cs.model.CoverageAnalysis;
+import edu.byu.cs.model.Rubric;
+import edu.byu.cs.model.TestNode;
+import edu.byu.cs.model.TestOutput;
+import edu.byu.cs.util.PhaseUtils;
 
 /**
  * Runs and scores the unit tests for the phase a submission is graded for
@@ -51,25 +54,60 @@ public class UnitTestGrader extends TestGrader {
 
         if (totalTests == 0) return 0;
 
-        int minTests = PhaseUtils.minUnitTests(gradingContext.phase());
+        if (testResults.getNumTestsFailed() > 0) {
+            return 0;
+        }
 
-        if (totalTests < minTests) return (float) testResults.getNumTestsPassed() / minTests;
+        float coveragePercent = getCoveragePercent(testOutput.coverage());
+        float targetPercent = 0.8F; // how much we want covered, change me if too low or high
 
-        return testResults.getNumTestsPassed() / totalTests;
+        if (coveragePercent > targetPercent){
+            return 1;
+        }
+
+        return coveragePercent / targetPercent;
     }
 
     @Override
     protected String getNotes(TestOutput testOutput) throws GradingException {
         TestNode testResults = testOutput.root();
-        Integer totalTestsRun = testResults.getNumTestsFailed() + testResults.getNumTestsPassed();
-        if (testResults.getNumTestsPassed() + testResults.getNumTestsFailed() < PhaseUtils.minUnitTests(gradingContext.phase()))
-            return "Not enough tests: each " + PhaseUtils.unitTestCodeUnderTest(gradingContext.phase()) +
-                    " method should have a positive and negative test";
-        return switch (testResults.getNumTestsFailed()) {
-            case 0 -> testResults.getNumTestsPassed() + "/" + totalTestsRun + " tests passed";
-            case 1 -> "1/" + totalTestsRun + " test failed";
-            default -> testResults.getNumTestsFailed() + "/" + totalTestsRun + " tests failed";
-        };
+        float coverage = getCoveragePercent(testOutput.coverage());
+        StringBuilder notes = new StringBuilder("Coverage: " + coverage*100);
+        notes.append("\n");
+        var pattern = Pattern.compile("^" + PhaseUtils.unitTestPackageForCoverage(gradingContext.phase()));
+        for (ClassCoverageAnalysis i : testOutput.coverage().classAnalyses()){
+            var matcher = pattern.matcher(i.packageName());
+            //TODO: make sure the class is in the covered package, probably include a phase util for this
+            if (matcher.find()){
+                var total = (i.covered() + i.missed());
+                notes.append(i.packageName())
+                        .append(".")
+                        .append(i.className())
+                        .append(":")
+                        .append(i.covered())
+                        .append("/")
+                        .append(total)
+                        .append("\n");
+            }
+        }
+        return notes.toString();
+        
+    }
+
+    private float getCoveragePercent(CoverageAnalysis coverage) throws GradingException{
+        float covered = 0;
+        float total = 0;
+        var pattern = Pattern.compile("^" + PhaseUtils.unitTestPackageForCoverage(gradingContext.phase()));
+        for (ClassCoverageAnalysis i : coverage.classAnalyses()){
+            var matcher = pattern.matcher(i.packageName());
+            //TODO: make sure the class is in the covered package, probably include a phase util for this
+            if (matcher.find()){
+                covered += i.covered();
+                total += (i.covered() + i.missed());
+            }
+        }
+
+        return covered / total;
     }
 
     @Override
