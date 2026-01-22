155805efee1d8fcbfa880bc001bab5a85de8fffc
mewilker <mewilk@byu.edu>
2026-01-22 15:39:21 -0700

    fix difference between class and file name

--- src/test/java/edu/byu/cs/service/AuthenticationServiceTests.java
+++ src/test/java/edu/byu/cs/service/AuthenticationServiceTests.java
@@ -26,7 +26,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 
-class AuthenticationServiceTest {
+class AuthenticationServiceTests {
 
     private UserDao mockUserDao;
     private User testUser;
